0d26bbb759247c3064e40438ce641ab535c692cd
Satish <satishazad@gmail.com>
2024-01-14 14:11:47 +0530

    Login page ui

--- frontend/commission_app/src/ui/pages/login/LoginPage.tsx
+++ frontend/commission_app/src/ui/pages/login/LoginPage.tsx
@@ -14,9 +14,19 @@ export const LoginPage = () => {
     const navigate = useNavigate();
 
     return (
-        <div>
+        <div style={{
+            // backgroundImage: `url(${backgroundImage})`,
+            // backgroundSize: 'cover',
+            // backgroundPosition: 'center',
+            // backgroundRepeat: 'no-repeat',
+        }}>
             <h1>Welcome to Login Page</h1>
-            <button onClick={() => navigate('/home')}>Login</button>
+            <div>
+
+            </div>
+            <div>
+
+            </div>
         </div>
     );
 }
