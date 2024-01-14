68958b6e17b56f66873ac5ffc63ab5165113d995
unknown <afnu@ourworldenergy.com>
2024-01-14 13:36:59 -0600

    MASIF: deployment changes

--- backend/scripts/make_postgres_image.sh
+++ backend/scripts/make_postgres_image.sh
@@ -7,5 +7,5 @@ Repo=`pwd`/..
 echo "Repo: $Repo"
 
 cd $Repo/scripts/
-docker build -f ../docker/Dockerfile-postgress -t postgres_pg14_custom:v1 .;
+docker build -f ../docker/Dockerfile-postgress -t postgres_db_latest:v1 .;
 
