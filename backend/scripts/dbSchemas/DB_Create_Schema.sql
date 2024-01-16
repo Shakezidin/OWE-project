65d6b2261fd168301ce9f6d5043bcef7004f24b2
unknown <afnu@ourworldenergy.com>
2024-01-16 17:04:34 -0600

    MASIF: settings tables added

--- backend/scripts/dbSchemas/DB_Create_Schema.sql
+++ backend/scripts/dbSchemas/DB_Create_Schema.sql
@@ -81,6 +81,8 @@ CREATE TABLE partners (
     partner_id serial NOT NULL,
     partner_name character varying,
     description character varying,
+    created_at timestamp without time zone NOT NULL,
+    updated_at timestamp without time zone NOT NULL,
     PRIMARY KEY (partner_id)
 );
 
@@ -88,6 +90,8 @@ CREATE TABLE sale_type (
     id serial NOT NULL,
     name character varying,
     description character varying,
+    created_at timestamp without time zone NOT NULL,
+    updated_at timestamp without time zone NOT NULL,
     PRIMARY KEY (id)
 );
 
@@ -103,6 +107,8 @@ CREATE TABLE commission_rates (
     rate character varying,
     start_date character varying NOT NULL,
     end_date character varying,
+    created_at timestamp without time zone NOT NULL,
+    updated_at timestamp without time zone NOT NULL,
     FOREIGN KEY (state_id) REFERENCES states(state_id),
     FOREIGN KEY (partner_id) REFERENCES partners(partner_id),
     FOREIGN KEY (installer_id) REFERENCES partners(partner_id),
@@ -110,8 +116,152 @@ CREATE TABLE commission_rates (
     PRIMARY KEY (id)
 );
 
+CREATE TABLE v_dealer (
+    id serial NOT NULL,
+    dealer_code character varying,
+    dealer_name character varying,
+    description character varying,
+    created_at timestamp without time zone NOT NULL,
+    updated_at timestamp without time zone NOT NULL,
+    PRIMARY KEY (id)
+);
+
+CREATE TABLE dealer_override (
+    id serial NOT NULL,
+    sub_dealer character varying,
+    dealer_id character varying,
+    pay_rate character varying,
+    start_date character varying NOT NULL,
+    end_date character varying,
+    created_at timestamp without time zone NOT NULL,
+    updated_at timestamp without time zone NOT NULL,
+    FOREIGN KEY (dealer_id) REFERENCES v_dealer(id),
+    PRIMARY KEY (id)
+);
+CREATE TABLE source (
+    id serial NOT NULL,
+    name character varying,
+    created_at timestamp without time zone NOT NULL,
+    updated_at timestamp without time zone NOT NULL,
+    PRIMARY KEY (id)
+);
+
+CREATE TABLE marketting_fees (
+    id serial NOT NULL,
+    source_id INT,
+    dba character varying,
+    state_id INT,
+    fee_rate character varying,
+    chg_dlr integer,
+    pay_src integer,
+    start_date character varying NOT NULL,
+    end_date character varying,
+    description character varying,
+    created_at timestamp without time zone NOT NULL,
+    updated_at timestamp without time zone NOT NULL,
+    FOREIGN KEY (state_id) REFERENCES states(state_id),
+    FOREIGN KEY (source_id) REFERENCES source(id),
+    PRIMARY KEY (id)
+);
+
+CREATE TABLE v_adders (
+    id serial NOT NULL,
+    adder_name character varying,
+    adder_type character varying,
+    price_type character varying,
+    price_amount character varying,
+    active integer,
+    description character varying,
+    created_at timestamp without time zone NOT NULL,
+    updated_at timestamp without time zone NOT NULL,
+    PRIMARY KEY (id)
+);
+
+CREATE TABLE loan_type (
+    id serial NOT NULL,
+    product_code  character varying,
+    active integer,
+    adder integer,
+    description character varying,
+    created_at timestamp without time zone NOT NULL,
+    updated_at timestamp without time zone NOT NULL,
+    PRIMARY KEY (id)
+);
 
 
+CREATE TABLE tier (
+    id serial NOT NULL,
+    tier character varying,
+    created_at timestamp without time zone NOT NULL,
+    updated_at timestamp without time zone NOT NULL,
+    PRIMARY KEY (id)
+);
 
+CREATE TABLE dealer_tier (
+    id serial NOT NULL,
+    dealer_id INT,
+    tier_id INT,
+    start_date character varying NOT NULL,
+    end_date character varying,
+    created_at timestamp without time zone NOT NULL,
+    updated_at timestamp without time zone NOT NULL,
+    FOREIGN KEY (tier_id) REFERENCES tier(id),
+    FOREIGN KEY (dealer_id) REFERENCES v_dealer(id),
+    PRIMARY KEY (id)
+);
+
+CREATE TABLE tier_loan_fee (
+    id serial NOT NULL,
+    dealer_tier INT,
+    installer_id INT,
+    state_id INT,
+    finance_type_name INT,
+    owe_cost character varying,
+    dlr_mu character varying,
+    dlr_cost character varying,
+    start_date character varying NOT NULL,
+    end_date character varying,
+    created_at timestamp without time zone NOT NULL,
+    updated_at timestamp without time zone NOT NULL,
+    FOREIGN KEY (dealer_tier) REFERENCES tier(id),
+    FOREIGN KEY (installer_id) REFERENCES partners(partner_id),
+    FOREIGN KEY (state_id) REFERENCES states(state_id),
+    FOREIGN KEY (finance_type_name) REFERENCES loan_type(finance_type_name),
+
+    PRIMARY KEY (id)
+);
+
+CREATE TABLE payment_schedule (
+    id serial NOT NULL,
+    rep_name character varying,
+    partner_id INT,
+    installer_id INT,
+    sale_type_id INT, 
+    state_id INT,
+    rl character varying,
+    draw character varying,
+    draw_max character varying,
+    rep_draw character varying,
+    rep_draw_max character varying,
+    rep_pay character varying,
+    FOREIGN KEY (state_id) REFERENCES states(state_id),
+    FOREIGN KEY (sale_type_id) REFERENCES sale_type(id),
+    FOREIGN KEY (installer_id) REFERENCES partners(partner_id),
+    FOREIGN KEY (partner_id) REFERENCES partners(partner_id),
+    PRIMARY KEY (id)
+);
+
+CREATE TABLE timeline_sla (
+    id serial NOT NULL,
+    type  character varying,
+    state_id INT,
+    days integer,
+    start_date character varying NOT NULL,
+    end_date character varying,
+    created_at timestamp without time zone NOT NULL,
+    updated_at timestamp without time zone NOT NULL,
+    FOREIGN KEY (state_id) REFERENCES states(state_id),
+    PRIMARY KEY (id)
+);
 
 /******************************SETTINGS DB TABLE END  ***********************************************/
