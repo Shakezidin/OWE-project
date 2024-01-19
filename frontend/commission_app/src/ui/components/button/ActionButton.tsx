fa694349ab454e5d8dc9950841de288ffb9cb377
Satish <satishazad@gmail.com>
2024-01-19 19:15:40 +0530

    Local changes.

---
+++ frontend/commission_app/src/ui/components/button/ActionButton.tsx
@@ -0,0 +1,23 @@
+/**
+ * Created by satishazad on 17/01/24
+ * File Name: ActionButton
+ * Product Name: WebStorm
+ * Project Name: commission_app
+ * Path: src/ui/components/button
+ */
+
+import React from "react";
+
+
+interface ActionButtonProps {
+
+}
+
+export const ActionButton = (props: ActionButtonProps) => {
+
+    return (
+        <button className={'loginButton'}>Log In</button>
+    )
+}
+
+
