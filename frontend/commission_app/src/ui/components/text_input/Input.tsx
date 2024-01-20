1c2e4386e5c83ed5e6a915de2c975595704856b2
Ankit chauhan <ankitchauhan1999@gmail.com>
2024-01-20 19:08:54 +0530

    UI fixes

--- frontend/commission_app/src/ui/components/text_input/Input.tsx
+++ frontend/commission_app/src/ui/components/text_input/Input.tsx
@@ -1,5 +1,6 @@
 import { ChangeEvent, FC } from "react";
 import "./Input.css";
+import { ReactComponent as EYE_ICON } from "../../../resources/assets/eye-icon.svg";
 
 interface InputProps {
   type: "text" | "number" | "email" | "password";
@@ -9,6 +10,7 @@ interface InputProps {
   disabled?: boolean;
   name?: string;
   onChange: (e: ChangeEvent<HTMLInputElement>) => void;
+  onClickEyeIcon?: (e: ChangeEvent<HTMLInputElement>) => void;
 }
 
 const Input: FC<InputProps> = ({
@@ -22,14 +24,18 @@ const Input: FC<InputProps> = ({
 }) => {
   return (
     <div className="input-wrapper">
-      <input
-        type={type}
-        id={name}
-        value={value}
-        placeholder={placeholder}
-        onChange={onChange}
-        disabled={disabled}
-      />
+      <div className="input-inner-view">
+        <input
+          type={type}
+          id={name}
+          value={value}
+          placeholder={placeholder}
+          onChange={onChange}
+          disabled={disabled}
+        />
+        {type === "password" ? <EYE_ICON className="eyeIcon" /> : <></>}
+      </div>
+
       {error && <p className="error">Input filed can't be empty!</p>}
     </div>
   );
