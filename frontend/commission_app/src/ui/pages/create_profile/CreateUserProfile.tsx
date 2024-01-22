79289a8947dfdcc0831b7d0d65392cfe1407a5c3
Ankit chauhan <ankitchauhan1999@gmail.com>
2024-01-22 21:07:19 +0530

    create UI model created

--- frontend/commission_app/src/ui/pages/create_profile/CreateUserProfile.tsx
+++ frontend/commission_app/src/ui/pages/create_profile/CreateUserProfile.tsx
@@ -3,72 +3,84 @@ import "./CreateUserProfile.css";
 import { ReactComponent as PROFILE_BACKGROUND } from "../../../resources/assets/Profile_background.svg";
 import { ActionButton } from "../../components/button/ActionButton";
 import Input from "../../components/text_input/Input";
+import { ReactComponent as CROSS_BUTTON } from "../../../resources/assets/cross_button.svg";
 
 const CreateUserProfile = () => {
   return (
     <div className="transparent-model">
       <div className="modal">
-        <span className="createProfileText">Create User Profile</span>
-        <span className="createProfileSubText">
-          Enter the below detail to create User New Profile
-        </span>
+        <div className="createUserCrossButton">
+          <CROSS_BUTTON />
+        </div>
+        <div className="createUserContainer">
+          <span className="createProfileText">Create User Profile</span>
+          <span className="createProfileSubText">
+            Enter the below detail to create User New Profile
+          </span>
 
-        <div className="createProfileInputView">
-          <div className="uploadImageView">
-            <PROFILE_BACKGROUND />
-            <br />
-            <button>Upload Photo</button>
-          </div>
-          <div className="createProfileTextView">
-            <div style={{ display: "flex", gap: "20px" }}>
-              <Input
-                type={"text"}
-                label="First Name*"
-                value={""}
-                placeholder={"Enter Email"}
-                onChange={() => {}}
-              />
-              <Input
-                type={"text"}
-                label="Last Name*"
-                value={""}
-                placeholder={"Enter Email"}
-                onChange={() => {}}
-              />
+          <div className="createProfileInputView">
+            <div className="uploadImageView">
+              <PROFILE_BACKGROUND />
+              <br />
+              <button>Upload Photo</button>
             </div>
+            <div className="createUserLine"></div>
+            <div className="createProfileTextView">
+              <div style={{ display: "flex", gap: "20px" }}>
+                <Input
+                  type={"text"}
+                  label="First Name*"
+                  value={""}
+                  placeholder={"Enter First Name"}
+                  onChange={() => {}}
+                />
+                <Input
+                  type={"text"}
+                  label="Last Name*"
+                  value={""}
+                  placeholder={"Enter Last Name"}
+                  onChange={() => {}}
+                />
+              </div>
 
-            <div style={{ display: "flex", gap: "20px" }}>
-              <Input
-                type={"text"}
-                label="Email ID*"
-                value={""}
-                placeholder={"Enter Email"}
-                onChange={() => {}}
-              />
-              <Input
-                type={"text"}
-                label="Phone Number*"
-                value={""}
-                placeholder={"Enter Email"}
-                onChange={() => {}}
-              />
-            </div>
+              <div style={{ display: "flex", gap: "20px" }}>
+                <Input
+                  type={"text"}
+                  label="Email ID*"
+                  value={""}
+                  placeholder={"Enter Email ID"}
+                  onChange={() => {}}
+                />
+                <Input
+                  type={"text"}
+                  label="Phone Number*"
+                  value={""}
+                  placeholder={"Enter Phone Number"}
+                  onChange={() => {}}
+                />
+              </div>
 
-            <div style={{ display: "flex", gap: "20px" }}>
-              <Input
-                type={"text"}
-                label="Select Role*"
-                value={""}
-                placeholder={"Enter Email"}
-                onChange={() => {}}
-              />
-              <Input
-                type={"text"}
-                label="Assign Dealer*"
-                value={""}
-                placeholder={"Enter Email"}
-                onChange={() => {}}
-              />
+              <div style={{ display: "flex", gap: "20px" }}>
+                <Input
+                  type={"text"}
+                  label="Select Role*"
+                  value={""}
+                  placeholder={"Enter Email"}
+                  onChange={() => {}}
+                />
+                <Input
+                  type={"text"}
+                  label="Assign Dealer*"
+                  value={""}
+                  placeholder={"Enter Email"}
+                  onChange={() => {}}
+                />
+              </div>
+              <br />
+              <div className="createUserActionButton">
+                <ActionButton title={"Create"} onClick={() => {}} />
+                <br />
+              </div>
             </div>
           </div>
         </div>
