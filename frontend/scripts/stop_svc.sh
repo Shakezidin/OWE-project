afc127fb85bb0b971a309e6e8b1ae010f0939fe1
Shushank Sharma <shushank22@gmail.com>
2024-01-23 19:21:18 +0530

    Add scripts and docker files for GUI deployment

---
+++ frontend/scripts/stop_svc.sh
@@ -0,0 +1,25 @@
+#!/bin/bash
+
+# Get the directory path where the script is located
+SCRIPT_DIR="$(cd "$(dirname "${BASH_SOURCE[0]}")" >/dev/null 2>&1 && pwd)"
+
+# Get the parent directory of the script's directory
+BASE_DIR="$(dirname "$SCRIPT_DIR")"
+
+DOCKER_PATH=$BASE_DIR/docker
+
+
+DOCKER_COMPOSE_FILE="$DOCKER_PATH/docker-compose.yml"
+echo "$DOCKER_COMPOSE_FILE"
+
+if [ ! -f "$DOCKER_COMPOSE_FILE" ]; then
+    echo "Cannot find the docker-compose.yml file $DOCKER_COMPOSE_FILE"
+    exit 1
+fi
+
+# Change directory to the location of the docker-compose.yml file
+cd "$DOCKER_PATH" || exit 1
+
+# Start the Docker containers using docker-compose
+docker-compose down
+
