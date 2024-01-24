3b3fef4466fb095148b00e619704a6948723a11d
Shushank Sharma <shushank22@gmail.com>
2024-01-24 09:41:46 +0530

    Restructre Folder

---
+++ commissions/scripts/buildApp_ui.sh
@@ -0,0 +1,31 @@
+#!/bin/bash
+
+AppRoot='../'
+
+function make_docker()
+{
+	cd $AppRoot/build
+	make -f Makefile-UI docker
+	cd -
+}
+
+function make_dockerclean()
+{
+    cd $AppRoot/build
+	make -f Makefile-UI dockerpushclean
+	cd -
+}
+
+if [ "$#" -eq 0 ] 
+then
+	echo "Usage ./buildApp.sh <arguments:make_docker, make_doockerclean>"
+elif [[ $1 == "make_docker" ]]
+then
+	echo $# $1
+	make_docker
+elif [[ $1 == "make_dockerclean" ]]
+then
+	moduleinit
+	make_dockerclean
+fi
+
