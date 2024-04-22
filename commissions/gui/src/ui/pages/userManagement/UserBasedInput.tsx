6853599d0c0d05d1eb386566b72d231f244fc3ae
Ankit@10112019 <ankitchauhan1999.work@gmail.com>
2024-04-22 20:01:40 +0530

    user mgt bug fixes

--- commissions/gui/src/ui/pages/userManagement/UserBasedInput.tsx
+++ commissions/gui/src/ui/pages/userManagement/UserBasedInput.tsx
@@ -1,87 +1,79 @@
-import React from 'react'
-import Input from '../../components/text_input/Input'
+import React from "react";
+import Input from "../../components/text_input/Input";
 
-interface inputSelectProps{
-    onChange:any,
-    createUserOnboarding:any,
-
+interface inputSelectProps {
+  onChange: any;
+  createUserOnboarding: any;
 }
 
-const UserBasedInput:React.FC<inputSelectProps> = ({createUserOnboarding,onChange}) => {
+const UserBasedInput: React.FC<inputSelectProps> = ({
+  createUserOnboarding,
+  onChange,
+}) => {
   return (
     <div className="create-input-container">
-  {createUserOnboarding?.role_name === "rm" && (
-    <div className="create-input-field">
-      <Input
-        type={"text"}
-        label="Add Region"
-        value={createUserOnboarding.add_region}
-        placeholder={"Add Region"}
-        onChange={(e) => onChange(e)}
-        name={"add_region"}
-      />
+      {createUserOnboarding?.role_name === "rm" && (
+        <div className="create-input-field">
+          <Input
+            type={"text"}
+            label="Add Region"
+            value={createUserOnboarding.add_region}
+            placeholder={"Add Region"}
+            onChange={(e) => onChange(e)}
+            name={"add_region"}
+          />
+        </div>
+      )}
+      {createUserOnboarding?.role_name === "sales_rep_m" && (
+        <>
+          <div className="create-input-field">
+            <Input
+              type={"text"}
+              label="Report To"
+              value={createUserOnboarding.report_to}
+              placeholder={"Report To"}
+              onChange={(e) => onChange(e)}
+              name={"report_to"}
+            />
+          </div>
+          <div className="create-input-field">
+            <Input
+              type={"text"}
+              label="Team Name"
+              value={createUserOnboarding.team_name}
+              placeholder={"Team Name"}
+              onChange={(e) => onChange(e)}
+              name={"team_name"}
+            />
+          </div>
+        </>
+      )}
+      {createUserOnboarding?.role_name === "sales_m" && (
+        <>
+          <div className="create-input-field">
+            <Input
+              type={"text"}
+              label="Add Region"
+              value={createUserOnboarding.add_region}
+              placeholder={"Add Region"}
+              onChange={(e) => onChange(e)}
+              name={"add_region"}
+            />
+          </div>
+          <div className="create-input-field">
+            <Input
+              type={"text"}
+              label="Reporting To"
+              value={createUserOnboarding.reporting_to}
+              placeholder={"Reporting To"}
+              onChange={(e) => onChange(e)}
+              name={"reporting_to"}
+            />
+          </div>
+        </>
+      )}
     </div>
-  )}
-  {createUserOnboarding?.role_name === "sales_rep_m" && (
-    <>
-      <div className="create-input-field">
-        <Input
-          type={"text"}
-          label="Report To"
-          value={createUserOnboarding.report_to}
-          placeholder={"Report To"}
-          onChange={(e) => onChange(e)}
-          name={"report_to"}
-        />
-      </div>
-      <div className="create-input-field">
-        <Input
-          type={"text"}
-          label="Team Name"
-          value={createUserOnboarding.team_name}
-          placeholder={"Team Name"}
-          onChange={(e) => onChange(e)}
-          name={"team_name"}
-        />
-      </div>
-    </>
-  )}
-  {createUserOnboarding?.role_name === "sales_m" && (
-    <>
-      <div className="create-input-field">
-        <Input
-          type={"text"}
-          label="Add Region"
-          value={createUserOnboarding.add_region}
-          placeholder={"Add Region"}
-          onChange={(e) => onChange(e)}
-          name={"add_region"}
-        />
-      </div>
-      <div className="create-input-field">
-        <Input
-          type={"text"}
-          label="Reporting To"
-          value={createUserOnboarding.reporting_to}
-          placeholder={"Reporting To"}
-          onChange={(e) => onChange(e)}
-          name={"reporting_to"}
-        />
-      </div>
-      <div className="create-input-field">
-        <Input
-          type={"text"}
-          label="Report To"
-          value={createUserOnboarding.report_to}
-          placeholder={"Report To"}
-          onChange={(e) => onChange(e)}
-          name={"report_to"}
-        />
-      </div>
-    </>
-  )}
-</div>
-  )
-}
+  );
+};
 
-export default UserBasedInput
+export default UserBasedInput;
