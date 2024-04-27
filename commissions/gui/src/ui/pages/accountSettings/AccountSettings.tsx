3a287546353110f31c99167d401c51d4239a0e90
Vinay Kumar <itsmevinay3150@gmail.com>
2024-04-27 15:30:35 +0530

    account setting

--- commissions/gui/src/ui/pages/accountSettings/AccountSettings.tsx
+++ commissions/gui/src/ui/pages/accountSettings/AccountSettings.tsx
@@ -1,5 +1,5 @@
 import React from "react";
-import { useState } from 'react'
+import { useState } from "react";
 import "./AccountSettings.css";
 import { ICONS } from "../../icons/Icons";
 import MyProfile from "./MyProfile";
@@ -7,13 +7,13 @@ import ResetPassword from "../resetPassword/ResetPassword";
 import ResetPasswordAccount from "./ResetPasswordAccount";
 import { logout } from "../../../redux/apiSlice/authSlice/authSlice";
 import { useAppDispatch } from "../../../redux/hooks";
-import { Link, useLocation, useNavigate } from 'react-router-dom';
+import { Link, useLocation, useNavigate } from "react-router-dom";
 
 const AccountSettings = () => {
   const [activeTab, setActiveTab] = useState(0);
   const dispatch = useAppDispatch();
   const navigate = useNavigate();
-  const handleTabClick = (i:number) => {
+  const handleTabClick = (i: number) => {
     setActiveTab(i);
   };
   const handleLogout = () => {
@@ -22,34 +22,47 @@ const AccountSettings = () => {
   };
   return (
     <>
-      <div className="">
-        <div className="account-container">
-          <div className="titlehed">
-            <img src={ICONS.settingIconAcc} alt="" />
-          </div>
-          <div className="">
-            <p> Account Setting</p>
-          </div>
+      <div className="account-container">
+        <div className="titlehed">
+          <img src={ICONS.settingIconAcc} alt="" />
+        </div>
+        <div className="">
+          <p> Account Setting</p>
         </div>
       </div>
-      <div className="profile-section">
-     
-        <div className="tab-links">
-          <button onClick={() => handleTabClick(0)} className={`tab ${activeTab===0?"active-profile":""}`}>My Profile</button>
-          <button onClick={() => handleTabClick(1)} className={`tab ${activeTab===1?"active-profile":""}`}>Reset Password</button>
-          <button onClick={handleLogout} className="tab">Logout</button>
+      <div className="account-section-main">
+        <div className="profile-section">
+          <div className="tab-links">
+            <button
+              onClick={() => handleTabClick(0)}
+              className={`tab ${activeTab === 0 ? "active-profile" : ""}`}
+            >
+              My Profile
+            </button>
+            <button
+              onClick={() => handleTabClick(1)}
+              className={`tab ${activeTab === 1 ? "active-profile" : ""}`}
+            >
+              Reset Password
+            </button>
+            <button onClick={handleLogout} className="tab">
+              Logout
+            </button>
+          </div>
+          <div className="vertical"></div>
+          <div className="tab-content">
+            {activeTab === 0 && (
+              <div>
+                <MyProfile />
+              </div>
+            )}
+            {activeTab === 1 && (
+              <div>
+                <ResetPasswordAccount />
+              </div>
+            )}
+          </div>
         </div>
-        <div className="vertical"></div>
-        <div className="tab-content">
-          {activeTab === 0 && <div><MyProfile /></div>}
-          {activeTab === 1 && <div><ResetPasswordAccount /></div>}
-  
-        </div>
-
-       
-
-        {/* <MyProfile /> */}
-        {/* <ResetPasswordAccount /> */}
       </div>
     </>
   );
