02a74fa070139e70b5f8620ff0438c6e1a800360
Vinay Kumar <itsmevinay3150@gmail.com>
2024-04-29 00:38:01 +0530

    changes done

--- commissions/gui/src/ui/pages/dashboard/HelpDashboard.tsx
+++ commissions/gui/src/ui/pages/dashboard/HelpDashboard.tsx
@@ -1,4 +1,4 @@
-import React,{useRef} from "react";
+import React from "react";
 import "../dashboard/dasboard.css";
 import { ICONS } from "../../icons/Icons";
 import Input from "../../components/text_input/Input";
@@ -15,17 +15,6 @@ const HelpDashboard: React.FC<ButtonProps> = ({
   handleClose,
   commission,
 }) => {
-  const fileInputRef = useRef<HTMLInputElement>(null);
-
-  const handleFileInputChange = (e: any) => {
-    const file = e.target.files?.[0];
-    console.log(file);
-  };
-
-
-  const handleButtonClick = () => {
-    fileInputRef.current?.click(); // Trigger file input click event
-  };
   return (
     <>
       <div className="transparent-model-down">
@@ -56,114 +45,99 @@ const HelpDashboard: React.FC<ButtonProps> = ({
                 />
               </div>
             </div>
-          </div>
-          <div className="create-input-container">
+
+            <div className="create-input-container">
+              <div className="create-input-field-help">
+                <Input
+                  type={"text"}
+                  label="Dealer Name"
+                  value={""}
+                  name="fee_rate"
+                  placeholder={"Enter"}
+                  onChange={(e) => {}}
+                />
+              </div>
+
+              <div className="create-input-field-help">
+                <Input
+                  type={"text"}
+                  label="Sale Rep."
+                  value={""}
+                  name="pay_src"
+                  placeholder={"Enter"}
+                  onChange={(e) => {}}
+                />
+              </div>
+              <div className="create-input-field-help">
+                <Input
+                  type={"text"}
+                  label="Customer Name"
+                  value={""}
+                  name="pay_src"
+                  placeholder={"Enter"}
+                  onChange={(e) => {}}
+                />
+              </div>
+            </div>
+
+            <div className="create-input-container">
+              <div className="create-input-field-help">
+                <Input
+                  type={"text"}
+                  label="Amount Prepaid"
+                  value={""}
+                  name="fee_rate"
+                  placeholder={"Enter"}
+                  onChange={(e) => {}}
+                />
+              </div>
+
+              <div className="create-input-field-help">
+                <Input
+                  type={"text"}
+                  label="Pipeline Remaining"
+                  value={""}
+                  name="pay_src"
+                  placeholder={"Enter"}
+                  onChange={(e) => {}}
+                />
+              </div>
+              <div className="create-input-field-help">
+                <Input
+                  type={"text"}
+                  label="Current Date"
+                  value={""}
+                  name="pay_src"
+                  placeholder={"Enter"}
+                  onChange={(e) => {}}
+                />
+              </div>
+            </div>
+            <div className="create-input-container">
+              <div className="create-input-field-help">
+                <Input
+                  type={"text"}
+                  label="Project Status"
+                  value={""}
+                  name="fee_rate"
+                  placeholder={"Enter"}
+                  onChange={(e) => {}}
+                />
+              </div>
+
+              <div className="create-input-field-help">
+                <Input
+                  type={"text"}
+                  label="State"
+                  value={""}
+                  name="pay_src"
+                  placeholder={"Enter"}
+                  onChange={(e) => {}}
+                />
+              </div>
+            </div>
+
             <div
-              className="create-input-field"
-              style={{width:"66%", padding:"0.5rem 2rem 0.5rem 2rem"}}
-            >
-              <Input
-                type={"text"}
-                label="Project ID"
-                value={""}
-                name="fee_rate"
-                placeholder={"Enter"}
-                onChange={(e) => { }}
-              />
-            </div>
-          </div>
-
-          <div className="create-input-container">
-            <div className="create-input-field-help">
-              <Input
-                type={"text"}
-                label="Dealer Name"
-                value={""}
-                name="fee_rate"
-                placeholder={"Enter"}
-                onChange={(e) => { }}
-              />
-            </div>
-
-            <div className="create-input-field-help">
-              <Input
-                type={"text"}
-                label="Sale Rep."
-                value={""}
-                name="pay_src"
-                placeholder={"Enter"}
-                onChange={(e) => { }}
-              />
-            </div>
-            <div className="create-input-field-help">
-              <Input
-                type={"text"}
-                label="Customer Name"
-                value={""}
-                name="pay_src"
-                placeholder={"Enter"}
-                onChange={(e) => { }}
-              />
-            </div>
-          </div>
-
-          <div className="create-input-container">
-            <div className="create-input-field-help">
-              <Input
-                type={"text"}
-                label="Amount Prepaid"
-                value={""}
-                name="fee_rate"
-                placeholder={"Enter"}
-                onChange={(e) => { }}
-              />
-            </div>
-
-            <div className="create-input-field-help">
-              <Input
-                type={"text"}
-                label="Pipeline Remaining"
-                value={""}
-                name="pay_src"
-                placeholder={"Enter"}
-                onChange={(e) => { }}
-              />
-            </div>
-            <div className="create-input-field-help">
-              <Input
-                type={"text"}
-                label="Current Date"
-                value={""}
-                name="pay_src"
-                placeholder={"Enter"}
-                onChange={(e) => { }}
-              />
-            </div>
-          </div>
-          <div className="create-input-container">
-            <div className="create-input-field-help">
-              <Input
-                type={"text"}
-                label="Project Status"
-                value={""}
-                name="fee_rate"
-                placeholder={"Enter"}
-                onChange={(e) => { }}
-              />
-            </div>
-
-            <div className="create-input-field-help">
-              <Input
-                type={"text"}
-                label="State"
-                value={""}
-                name="pay_src"
-                placeholder={"Enter"}
-                onChange={(e) => { }}
-              />
-            </div>
-            <div
-
               className="create-input-field-note"
               style={{  }}
             >
@@ -171,22 +145,21 @@ const HelpDashboard: React.FC<ButtonProps> = ({
                 htmlFor=""
                 className="inputLabel"
               
-
               >
-              <label htmlFor="" className="inputLabel">
                 Message
               </label>
               <br />
               <textarea
-                name="message"
+                name={""}
                 id=""
                 rows={4}
-                // value={message}
+                onChange={(e) => {}}
+                value={""}
                 placeholder="Type here..."
+                
               ></textarea>
-              
-              
-                </div>
+            </div>
+
           
           </div>
        </div>
@@ -201,11 +174,9 @@ const HelpDashboard: React.FC<ButtonProps> = ({
               />
               <ActionButton title={"Submit"} type="submit" onClick={() => {}} />
             </div>
-
           </div>
         </div>
-      </div>
-
+   
     </>
   );
 };
