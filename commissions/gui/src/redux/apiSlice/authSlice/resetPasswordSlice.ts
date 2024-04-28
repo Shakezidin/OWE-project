df6e8e80b6eb2417a550017a026452cbbdba57e1
Ankit@10112019 <ankitchauhan1999.work@gmail.com>
2024-04-28 16:47:22 +0530

    OTP send done

--- commissions/gui/src/redux/apiSlice/authSlice/resetPasswordSlice.ts
+++ commissions/gui/src/redux/apiSlice/authSlice/resetPasswordSlice.ts
@@ -1,8 +1,8 @@
-import { createAsyncThunk, createSlice } from "@reduxjs/toolkit";
-import { postCaller } from "../../../infrastructure/web_api/services/apiUrl";
-import { EndPoints } from "../../../infrastructure/web_api/api_client/EndPoints";
+import { createSlice } from "@reduxjs/toolkit";
+import { generateOTP } from "../../apiActions/authActions";
+import { stat } from "fs";
 
-interface resetPasswordState {
+export interface ResetPasswordModel {
   email: string;
   loading?: boolean;
   error?: string | null;
@@ -10,45 +10,40 @@ interface resetPasswordState {
   new_password?: string;
   message?: string;
 }
-const initialState: resetPasswordState = {
-  email: "",
+const initialState: ResetPasswordModel = {
   loading: false,
   error: null,
+  email: "",
   message: "",
   otp: "",
   new_password: "",
 };
 
-const ACTION_PREFIX = "AUTH";
-const TYPE_PREFIX = ACTION_PREFIX + "/forgotPassword";
-export const generateOTP = createAsyncThunk(TYPE_PREFIX, async (data: any) => {
-  console.log("email....", data);
-  const response = await postCaller(EndPoints.resetPassword, data);
-
-  console.log(response);
-
-  return response;
-});
-
 const resetPasswordSlice = createSlice({
   name: "resetPassword",
   initialState,
-  reducers: {},
+  reducers: {
+    updateEmail(state, action) {
+      state.email = action.payload;
+    },
+  },
   extraReducers: (builder) => {
-    builder.addCase(generateOTP.pending, (state) => {
-      state.loading = true;
-      state.error = null;
-    });
-    builder.addCase(generateOTP.fulfilled, (state, action) => {
-      state.loading = false;
-      state.error = null;
-      state.message = action.payload.message;
-    });
-    builder.addCase(generateOTP.rejected, (state, action) => {
-      state.loading = false;
-      state.error = action.error.message ?? "something went wrong";
-    });
+    builder
+      .addCase(generateOTP.pending, (state) => {
+        state.loading = true;
+        state.error = null;
+      })
+      .addCase(generateOTP.fulfilled, (state, action) => {
+        state.loading = false;
+        state.error = null;
+        const { message } = action.payload;
+        state.message = message;
+      })
+      .addCase(generateOTP.rejected, (state, action) => {
+        state.loading = false;
+        state.error = action.error.message ?? "something went wrong";
+      });
   },
 });
-
+export const { updateEmail } = resetPasswordSlice.actions;
 export default resetPasswordSlice.reducer;
