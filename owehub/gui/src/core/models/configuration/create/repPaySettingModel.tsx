559b0f1ca2180ddd729ca3bdf158884f788ee82c
Raman <rajudeveloperw892@gmail.com>
2024-05-01 20:48:24 +0530

    changes

---
+++ owehub/gui/src/core/models/configuration/create/repPaySettingModel.tsx
@@ -0,0 +1,9 @@
+export interface repPaySettingModel {
+    name:string,
+    state: string;
+    pay_scale: string;
+    position: string;
+    be: string;
+    start: string;
+    end: string;
+  }
