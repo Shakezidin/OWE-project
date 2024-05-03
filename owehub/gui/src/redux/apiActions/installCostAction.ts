36402bbc30f909bca005bc7f4051163416065b08
Ankit Gupta <ankit@hexabells.com>
2024-05-03 15:45:21 +0530

    added instace error

--- owehub/gui/src/redux/apiActions/installCostAction.ts
+++ owehub/gui/src/redux/apiActions/installCostAction.ts
@@ -1,53 +1,58 @@
 import { createAsyncThunk } from "@reduxjs/toolkit";
 import { postCaller } from "../../infrastructure/web_api/services/apiUrl";
 interface Ipaginate {
-    page_number: number,
-    page_size: number
+  page_number: number;
+  page_size: number;
 }
 
 export interface ICostCreateparam {
-    unique_id:  string;
-    cost:       number;
-    start_date: string;
-    end_date:   string;
+  unique_id: string;
+  cost: number;
+  start_date: string;
+  end_date: string;
 }
-
 
 export interface ICost extends ICostCreateparam {
-    record_id: string;
+  record_id: string;
 }
 
-
-
-export const getInstallCost = createAsyncThunk("fetch/installCost",async(param:Ipaginate,{rejectWithValue})=>{
+export const getInstallCost = createAsyncThunk(
+  "fetch/installCost",
+  async (param: Ipaginate, { rejectWithValue }) => {
     try {
-        const data = await postCaller("get_installcost",param)
-        return data.data.install_cost_list || [] as ICost[]
+      const data = await postCaller("get_installcost", param);
+      return data.data.install_cost_list || ([] as ICost[]);
     } catch (error) {
-        return rejectWithValue((error as Error).message)
+      return rejectWithValue((error as Error).message);
     }
-})
+  }
+);
 
-export const createInstallCost = createAsyncThunk("create/installCost",async(params:ICostCreateparam,{rejectWithValue})=>{
+export const createInstallCost = createAsyncThunk(
+  "create/installCost",
+  async (params: ICostCreateparam, { rejectWithValue }) => {
     try {
-        const data = await postCaller("create_installCost",params)
-        return data
+      const data = await postCaller("create_installCost", params);
+      if (data instanceof Error) {
+        return rejectWithValue((data as Error).message);
+      }
+      return data;
     } catch (error) {
-        return rejectWithValue((error as Error).message)
+      return rejectWithValue((error as Error).message);
     }
-
-})
+  }
+);
 
 // update_installcost
 
-
-
-export const updateInstallCost = createAsyncThunk("update/installCost",async(params:ICost,{rejectWithValue})=>{
+export const updateInstallCost = createAsyncThunk(
+  "update/installCost",
+  async (params: ICost, { rejectWithValue }) => {
     try {
-        const data = await postCaller("update_installcost",params)
-        return data
+      const data = await postCaller("update_installcost", params);
+      return data;
     } catch (error) {
-        return rejectWithValue((error as Error).message)
+      return rejectWithValue((error as Error).message);
     }
-
-})
+  }
+);
