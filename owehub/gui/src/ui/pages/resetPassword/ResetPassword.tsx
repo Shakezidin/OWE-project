b4f40af8d59249592d8cd15db92814264a4a2a3e
Vance-Developer <vance.developer@gmail.com>
2024-05-08 15:12:02 +0530

    Browser-compat mac

--- owehub/gui/src/ui/pages/resetPassword/ResetPassword.tsx
+++ owehub/gui/src/ui/pages/resetPassword/ResetPassword.tsx
@@ -75,7 +75,7 @@ const ResetPassword = () => {
         <div className={"loginBox2"}>
           <form onSubmit={(e) => handleEmailSubmit(e)}>
             <div className="loginTextView">
-              <img className="loginImageLogo" src={ICONS.LOGO} alt="" />
+            <object type="image/svg+xml" className="loginImageLogo"  data={ICONS.LOGO} aria-label="login-icon"></object>
               <br />
               <div className="loginLogowithText">
                 <LOGO_SMALL />
