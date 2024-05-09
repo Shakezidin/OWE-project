389d452151dce93fd3a2e720e40d9449b0f4202c
Shakezidin <sinuzidin@gmail.com>
2024-05-09 13:09:53 +0530

    small comma error fixed

--- owehub/scripts/dbSchemas/DB_Create_Config_Schema.sql
+++ owehub/scripts/dbSchemas/DB_Create_Config_Schema.sql
@@ -468,7 +468,7 @@ CREATE TABLE referral_bonus (
 
 CREATE TABLE auto_adder (
     id serial NOT NULL,
-    unique_id varchar NOT NULL UNIQUE,,
+    unique_id varchar NOT NULL UNIQUE,
     date date,
     type text,
     gc text,
