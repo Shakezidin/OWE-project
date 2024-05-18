e358cbbb82b2969a9a7cf16a1183b2027403854e
Saurabh <risabht043@gmail.com>
2024-05-18 12:18:10 +0530

    add tooltip

--- owehub/gui/src/ui/pages/databaseManager/userActivity/UserActivity.tsx
+++ owehub/gui/src/ui/pages/databaseManager/userActivity/UserActivity.tsx
@@ -9,6 +9,9 @@ import { getCurrentDateFormatted } from '../../../../utiles/formatDate';
 import { DBManagerUserActivityModel } from '../../../../core/models/api_models/DBManagerModel';
 import DataNotFound from '../../../components/loader/DataNotFound';
 import Pagination from '../../../components/pagination/Pagination';
+import { Tooltip as ReactTooltip } from 'react-tooltip';
+import './Useractivity.css'
+import { createPortal } from 'react-dom';
 
 const UserActivity: React.FC = () => {
   const dispatch = useAppDispatch();
@@ -17,6 +20,20 @@ const UserActivity: React.FC = () => {
   );
   const [currentPage, setCurrentPage] = useState(1);
   const itemsPerPage = 10;
+  const [openTooltipIndex, setOpenTooltipIndex] = useState<number | null>(null);
+  useEffect(() => {
+    const handleClickOutside = (event: MouseEvent) => {
+      if (openTooltipIndex !== null && !(event.target as HTMLElement).closest(`[data-tooltip-id="tooltip-${openTooltipIndex}"]`)) {
+        setOpenTooltipIndex(null);
+      }
+    };
+
+    document.addEventListener('click', handleClickOutside);
+
+    return () => {
+      document.removeEventListener('click', handleClickOutside);
+    };
+  }, [openTooltipIndex]);
 
   useEffect(() => {
     const pageNumber = {
@@ -40,7 +57,7 @@ const UserActivity: React.FC = () => {
     setCurrentPage(currentPage - 1);
   };
 
-  const filter = () => {};
+  const filter = () => { };
 
   if (loading) {
     return <div>Loading...</div>;
@@ -49,6 +66,10 @@ const UserActivity: React.FC = () => {
   // if (error) {
   //   return <div>Error: {error}</div>;
   // }
+
+  const countWords = (str: string): number => {
+    return str.trim().split(/\s+/).length;
+  };
 
   const totalPages = Math.ceil(totalCount / itemsPerPage);
   const startIndex = (currentPage - 1) * itemsPerPage + 1;
@@ -99,25 +120,129 @@ const UserActivity: React.FC = () => {
               </tr>
             </thead>
 
+            {/* <tbody>
+              {userActivityList && userActivityList?.length > 0 ? (
+                userActivityList?.map((el: DBManagerUserActivityModel, index: number) => {
+                  // if (el.query_details === ';') {
+                  //   return null; // Skip rendering the row
+                  // }
+                  const tooltipId = `tooltip-${index}`;
+                  const truncatedQueryDetails = el.query_details.slice(0, 50) + '...';
+                  const wordCount = countWords(el.query_details);
+
+                 
+
+                  return (
+                    <tr key={el.time_date}>
+                      <td
+                        style={{
+                          fontWeight: '500',
+                          color: 'black',
+                          paddingLeft: '10px',
+                          textAlign: 'left',
+                        }}
+                      >
+                        {el.username}
+                      </td>
+                      <td style={{ textAlign: 'left' }}>{el.db_name}</td>
+                      <td style={{ textAlign: 'left' }}>{el.time_date}</td>
+                      <td style={{ textAlign: 'left' }}>
+                        {wordCount <= 5 ? el.query_details : truncatedQueryDetails}
+                        {wordCount > 5 && (
+                          <>
+                            <button
+                              onClick={() => setOpenTooltipIndex(openTooltipIndex === index ? null : index)}
+                              data-tooltip-id={tooltipId}
+                              data-tooltip-content={el.query_details}
+                              data-tooltip-place="bottom"
+                              style={{
+                                marginLeft: '5px',
+                                border: 'none',
+                                background: 'none',
+                                color: 'blue',
+                                cursor: 'pointer',
+                              }}
+                            >
+                              {openTooltipIndex === index ? 'Show less' : 'Show more'}
+                            </button>
+                            <ReactTooltip
+                              id={tooltipId}
+                              className="custom-tooltip"
+                              isOpen={openTooltipIndex === index}
+                            />
+                          </>
+                        )}
+                      </td>
+                    </tr>
+                  );
+                })
+              ) : (
+                <tr style={{ border: 0 }}>
+                  <td colSpan={10}>
+                    <div className="data-not-found">
+                      <DataNotFound />
+                      <h3>Data Not Found</h3>
+                    </div>
+                  </td>
+                </tr>
+              )}
+            </tbody> */}
+
             <tbody>
               {userActivityList && userActivityList?.length > 0 ? (
-                userActivityList?.map((el: DBManagerUserActivityModel) => (
-                  <tr key={el.time_date}>
-                    <td
-                      style={{
-                        fontWeight: '500',
-                        color: 'black',
-                        paddingLeft: '10px',
-                        textAlign: 'left',
-                      }}
-                    >
-                      {el.username}
-                    </td>
-                    <td style={{ textAlign: 'left' }}>{el.db_name}</td>
-                    <td style={{ textAlign: 'left' }}>{el.time_date}</td>
-                    <td style={{ textAlign: 'left' }}>{el.query_details}</td>
-                  </tr>
-                ))
+                userActivityList
+                  ?.filter((el: DBManagerUserActivityModel) => el.query_details !== ';') // Filter out rows where query_details is ';'
+                  ?.map((el: DBManagerUserActivityModel, index: number) => {
+                    const tooltipId = `tooltip-${index}`;
+                    const truncatedQueryDetails = el.query_details.slice(0, 50) + '...';
+                    const wordCount = countWords(el.query_details);
+
+
+
+                    return (
+                      <tr key={el.time_date}>
+                        <td
+                          style={{
+                            fontWeight: '500',
+                            color: 'black',
+                            paddingLeft: '10px',
+                            textAlign: 'left',
+                          }}
+                        >
+                          {el.username}
+                        </td>
+                        <td style={{ textAlign: 'left' }}>{el.db_name}</td>
+                        <td style={{ textAlign: 'left' }}>{el.time_date}</td>
+                        <td style={{ textAlign: 'left' }}>
+                          {wordCount <= 5 ? el.query_details : truncatedQueryDetails}
+                          {wordCount > 5 && (
+                            <>
+                              <button
+                                onClick={() => setOpenTooltipIndex(openTooltipIndex === index ? null : index)}
+                                data-tooltip-id={tooltipId}
+                                data-tooltip-content={el.query_details}
+                                data-tooltip-place="bottom"
+                                style={{
+                                  marginLeft: '5px',
+                                  border: 'none',
+                                  background: 'none',
+                                  color: 'blue',
+                                  cursor: 'pointer',
+                                }}
+                              >
+                                {openTooltipIndex === index ? 'Show less' : 'Show more'}
+                              </button>
+                              <ReactTooltip
+                                id={tooltipId}
+                                className="custom-tooltip"
+                                isOpen={openTooltipIndex === index}
+                              />
+                            </>
+                          )}
+                        </td>
+                      </tr>
+                    );
+                  })
               ) : (
                 <tr style={{ border: 0 }}>
                   <td colSpan={10}>
@@ -129,6 +254,7 @@ const UserActivity: React.FC = () => {
                 </tr>
               )}
             </tbody>
+
           </table>
         </div>
 
