cc987af31b5cb02bef3b0028e09331aaa50ffe0c
raedmajeed <raedam786@gmail.com>
2024-05-22 16:04:52 +0000

    fixed dealer_credit & loan_fee

--- owehub/shared/models/getDealerCreditApiModels.go
+++ owehub/shared/models/getDealerCreditApiModels.go
@@ -9,17 +9,13 @@ package models
 type GetDealerCredit struct {
 	RecordId    int64   `json:"record_id"`
 	UniqueID    string  `json:"unique_id"`
-	Customer    string  `json:"customer"`
-	DealerName  string  `json:"dealer_name"`
-	DealerDBA   string  `json:"dealer_dba"`
-	ExactAmount string  `json:"exact_amtount"`
+	ExactAmount float64  `json:"exact_amount"`
 	PerKWAmount float64 `json:"per_kw_amount"`
 	ApprovedBy  string  `json:"approved_by"`
 	Notes       string  `json:"notes"`
 	TotalAmount float64 `json:"total_amount"`
 	SysSize     float64 `json:"sys_size"`
-	StartDate   string  `json:"start_date"`
-	EndDate     *string `json:"end_date"`
+	Date        string  `json:"date"`
 }
 
 type GetDealerCreditList struct {
