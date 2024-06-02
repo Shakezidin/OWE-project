9cc944c4ba60d2c3c0ce6f4829d756ca8274ec7e
raedmajeed <raedam786@gmail.com>
2024-06-02 06:09:57 +0000

    Revert "raw data updated"
    This reverts commit b9a3a5f5909a240538844b754e7c2df84d72090d.

--- owehub/scripts/dbSchemas/DB_Create_Schema.sql
+++ owehub/scripts/dbSchemas/DB_Create_Schema.sql
@@ -62,12 +62,12 @@ INSERT INTO loan_type (product_code,active,adder,description) VALUES ('P123',1,1
 \copy install_cost(cost,start_date,end_date) FROM '/docker-entrypoint-initdb.d/install_cost.csv' DELIMITER ',' CSV;
 \copy reconcile(unique_id,start_date,amount,notes) FROM '/docker-entrypoint-initdb.d/reconcile.csv' DELIMITER ',' CSV;
 \copy adder_data(unique_id,date,type_ad_mktg,gc,exact_amount,type1,per_kw_amt,rep_percent,description,notes,sys_size,adder_cal) FROM '/docker-entrypoint-initdb.d/adder_data.csv' DELIMITER '^' CSV;
-\copy referral_data(unique_id,new_customer,referrer_serial,referrer_name,amount,rep_doll_divby_per,notes,type,sys_size,state_id,adder_amount,start_date,end_date) FROM '/docker-entrypoint-initdb.d/referral_data.csv' DELIMITER ',' CSV;
-\copy rebate_data(unique_id,customer_verf,type_rd_mktg,item,amount,rep_doll_divby_per,notes,type,sys_size,state_id,adder_amount,start_date,end_date) FROM '/docker-entrypoint-initdb.d/rebate_data.csv' DELIMITER ',' CSV;
+\copy referral_data(unique_id,new_customer,referrer_serial,referrer_name,amount,rep_doll_divby_per,notes,type,sys_size,state_id,adder_amount,start_date,end_date) FROM '/docker-entrypoint-initdb.d/referral_data.csv' DELIMITER '^' CSV;
+\copy rebate_data(unique_id,customer_verf,type_rd_mktg,item,amount,rep_doll_divby_per,notes,type,sys_size,state_id,adder_amount,start_date,end_date) FROM '/docker-entrypoint-initdb.d/rebate_data.csv' DELIMITER '^' CSV;
 \copy dealer_credit(unique_id,date,exact_amount,per_kw_amount,approved_by,notes,total_amount,sys_size) FROM '/docker-entrypoint-initdb.d/dealer_credit.csv' DELIMITER '^' CSV;
-\copy ap_rep(unique_id,rep,dba,type,date,amount) FROM '/docker-entrypoint-initdb.d/ap_rep.csv' DELIMITER ',' CSV;
-\copy payment_schedule(rep_id,partner_id,installer_id,sale_type_id,state_id,rl,draw,draw_max,rep_draw,rep_draw_max,rep_pay,start_date,end_date) FROM '/docker-entrypoint-initdb.d/pay_schedule.csv' DELIMITER ',' CSV;
-\copy ap_dealer(unique_id,dealer_id,dba,type,date,amount,method,transaction,notes) FROM '/docker-entrypoint-initdb.d/ap_dealer.csv' DELIMITER ',' CSV;
+\copy ap_rep(unique_id,rep,dba,type,date,amount) FROM '/docker-entrypoint-initdb.d/ap_rep.csv' DELIMITER '^' CSV;
+\copy payment_schedule(rep_id,partner_id,installer_id,sale_type_id,state_id,rl,draw,draw_max,rep_draw,rep_draw_max,rep_pay,start_date,end_date) FROM '/docker-entrypoint-initdb.d/pay_schedule.csv' DELIMITER '^' CSV;
+\copy ap_dealer(unique_id,dealer_id,dba,type,date,amount,method,transaction,notes) FROM '/docker-entrypoint-initdb.d/ap_dealer.csv' DELIMITER '^' CSV;
 
 /******************************SETTINGS DB TABLE END  ***********************************************/
 
