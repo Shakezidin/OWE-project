9487f4646a9c436b7fcf232178a5bd131aeda724
Shakezidin <sinuzidin@gmail.com>
2024-06-13 12:47:19 +0530

    conflict 3 solved

--- owehub/owehub-calc/reppaycalc/repPayCalcFunctions.go
+++ owehub/owehub-calc/reppaycalc/repPayCalcFunctions.go
@@ -12,23 +12,6 @@ import (
 * DESCRIPTION:     calculates the "contract$$" value based on the provided data
 * RETURNS:         gross revenue
 *****************************************************************************/
-func CalculateContractDolDol(netEpc float64, contract float64, sysSize float64) (contractdoldol float64) {
-	log.FuncErrorTrace(0, "netEpc %v  contract %v sysSize %v", netEpc, contract, sysSize)
-	if netEpc > 0 {
-		if contract > 0 {
-			contractdoldol = contract
-		} else {
-			contractdoldol = netEpc * 1000 * sysSize
-		}
-	}
-	return contractdoldol
-}
-
-/******************************************************************************
-* FUNCTION:        CalculateContractDolDol
-* DESCRIPTION:     calculates the "contract$$" value based on the provided data
-* RETURNS:         gross revenue
-*****************************************************************************/
 func CalculateAdderLf(dealer string, addr, expence, autoadder, loanfee, rebate, referral float64) (adderlf float64) {
 	log.FuncErrorTrace(0, "addr : %v expence : %v autoadder : %v loanFee : %v rabate : %v referral : %v", addr, expence, autoadder, loanfee, rebate, referral)
 	if len(dealer) > 0 {
@@ -49,32 +32,6 @@ func calculateAdderPerKW(dealer string, adderLF, sysSize float64) (adderPerKW fl
 		adderPerKW = adderLF / sysSize
 	}
 	return adderPerKW
-}
-
-/******************************************************************************
-* FUNCTION:        CalculateContractDolDol
-* DESCRIPTION:     calculates the "contract$$" value based on the provided data
-* RETURNS:         gross revenue
-*****************************************************************************/
-func CalculatePayRateSemi(dealer, commission_models, rep_1 string, epcCalc, rl, systemSize, netEpc float64, wc time.Time) (payRateSemi float64) {
-	date, _ := time.Parse("2006,01,02", "2023,12,20")
-	if commission_models == "standard" {
-		if len(dealer) > 0 {
-			return (epcCalc - rl) * 1000
-		}
-		return 0
-	} else {
-		if len(dealer) > 0 && systemSize > 0 {
-			if rep_1 == "Scott McCollester" && wc.After(date) {
-				return 1.88 * (systemSize * 1000)
-			} else if dealer == "Onyx D2D" {
-				return (netEpc - 1.98) * systemSize
-			} else {
-				return rl * (systemSize * 1000)
-			}
-		}
-	}
-	return 0
 }
 
 /******************************************************************************
