1d2cee393fc7bb759d2386d6484c0edf01520bef
Ankit Gupta <ankit@owe.com>
2024-07-11 17:13:08 +0530

    removed column

--- owehub/gui/src/resources/static_data/configureHeaderData/apPdaColumn.tsx
+++ owehub/gui/src/resources/static_data/configureHeaderData/apPdaColumn.tsx
@@ -45,10 +45,5 @@ export const ApPdaColumn: Column[] = [
     type: 'string',
     isCheckbox: false,
   }, 
-  {
-    name: 'description',
-    displayName: 'Description',
-    type: 'string',
-    isCheckbox: false,
-  },
+
 ];
