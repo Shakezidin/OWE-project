0be4d09bdcd6d8a4b47eace26be0e0c39123570d
Shakezidin <sinuzidin@gmail.com>
2024-07-12 17:18:28 +0530

    referaal data issue solved

--- owehub/owehub-main/services/apiCreateReferralData.go
+++ owehub/owehub-main/services/apiCreateReferralData.go
@@ -57,13 +57,8 @@ func HandleCreateReferralDataRequest(resp http.ResponseWriter, req *http.Request
 
 	if len(createReferralData.UniqueID) == 0 || len(createReferralData.NewCustomer) == 0 ||
 		len(createReferralData.ReferrerSerial) == 0 || len(createReferralData.ReferrerName) == 0 ||
-		len(createReferralData.Amount) == 0 || len(createReferralData.Notes) == 0 ||
-		len(createReferralData.Type) == 0 || len(createReferralData.Rep1Name) == 0 ||
-		len(createReferralData.Rep2Name) == 0 || len(createReferralData.State) == 0 ||
-		len(createReferralData.R1ReferralCredit) == 0 || len(createReferralData.R1ReferralCreditPerc) == 0 ||
-		len(createReferralData.R1AddrResp) == 0 || len(createReferralData.R2ReferralCredit) == 0 ||
-		len(createReferralData.R2ReferralCreditPerc) == 0 || len(createReferralData.R2AddrResp) == 0 ||
-		len(createReferralData.StartDate) == 0 || createReferralData.EndDate == nil || len(*createReferralData.EndDate) == 0 {
+		len(createReferralData.Notes) == 0 ||
+		len(createReferralData.Date) == 0 {
 		err = fmt.Errorf("Empty Input Fields in API is Not Allowed")
 		log.FuncErrorTrace(0, "%v", err)
 		FormAndSendHttpResp(resp, "Empty Input Fields in API is Not Allowed", http.StatusBadRequest, nil)
@@ -77,45 +72,10 @@ func HandleCreateReferralDataRequest(resp http.ResponseWriter, req *http.Request
 		return
 	}
 
-	if createReferralData.SysSize <= float64(0) {
-		err = fmt.Errorf("Invalid Sys Size value: %f, Not Allowed", createReferralData.SysSize)
+	if createReferralData.Amount <= float64(0) {
+		err = fmt.Errorf("Invalid amount price: %f, Not Allowed", createReferralData.Amount)
 		log.FuncErrorTrace(0, "%v", err)
-		FormAndSendHttpResp(resp, "Invalid Sys Size value Not Allowed", http.StatusBadRequest, nil)
-		return
-	}
-
-	if createReferralData.RepCount < float64(0) {
-		err = fmt.Errorf("Invalid Rep Count value: %f, Not Allowed", createReferralData.RepCount)
-		log.FuncErrorTrace(0, "%v", err)
-		FormAndSendHttpResp(resp, "Invalid Rep Count value Not Allowed", http.StatusBadRequest, nil)
-		return
-	}
-
-	if createReferralData.PerRepAddrShare < float64(0) {
-		err = fmt.Errorf("Invalid PerRepAddrShare value: %f, Not Allowed", createReferralData.PerRepAddrShare)
-		log.FuncErrorTrace(0, "%v", err)
-		FormAndSendHttpResp(resp, "Invalid PerRepAddrShare value Not Allowed", http.StatusBadRequest, nil)
-		return
-	}
-
-	if createReferralData.PerRepOvrdShare < float64(0) {
-		err = fmt.Errorf("Invalid PerRepOvrdShare value: %f, Not Allowed", createReferralData.PerRepOvrdShare)
-		log.FuncErrorTrace(0, "%v", err)
-		FormAndSendHttpResp(resp, "Invalid PerRepOvrdShare value Not Allowed", http.StatusBadRequest, nil)
-		return
-	}
-
-	if createReferralData.R1PayScale <= float64(0) {
-		err = fmt.Errorf("Invalid R1 Pay Scale value: %f, Not Allowed", createReferralData.R1PayScale)
-		log.FuncErrorTrace(0, "%v", err)
-		FormAndSendHttpResp(resp, "Invalid R1 Pay Scale value Not Allowed", http.StatusBadRequest, nil)
-		return
-	}
-
-	if createReferralData.R2PayScale <= float64(0) {
-		err = fmt.Errorf("Invalid R2 Pay Scale value: %f, Not Allowed", createReferralData.R2PayScale)
-		log.FuncErrorTrace(0, "%v", err)
-		FormAndSendHttpResp(resp, "Invalid R2 Pay Scale value Not Allowed", http.StatusBadRequest, nil)
+		FormAndSendHttpResp(resp, "Invalid RepDollDivbyPer price Not Allowed", http.StatusBadRequest, nil)
 		return
 	}
 
@@ -127,24 +87,7 @@ func HandleCreateReferralDataRequest(resp http.ResponseWriter, req *http.Request
 	queryParameters = append(queryParameters, createReferralData.Amount)
 	queryParameters = append(queryParameters, createReferralData.RepDollDivbyPer)
 	queryParameters = append(queryParameters, createReferralData.Notes)
-	queryParameters = append(queryParameters, createReferralData.Type)
-	queryParameters = append(queryParameters, createReferralData.Rep1Name)
-	queryParameters = append(queryParameters, createReferralData.Rep2Name)
-	queryParameters = append(queryParameters, createReferralData.SysSize)
-	queryParameters = append(queryParameters, createReferralData.RepCount)
-	queryParameters = append(queryParameters, createReferralData.State)
-	queryParameters = append(queryParameters, createReferralData.PerRepAddrShare)
-	queryParameters = append(queryParameters, createReferralData.PerRepOvrdShare)
-	queryParameters = append(queryParameters, createReferralData.R1PayScale)
-	queryParameters = append(queryParameters, createReferralData.R1ReferralCredit)
-	queryParameters = append(queryParameters, createReferralData.R1ReferralCreditPerc)
-	queryParameters = append(queryParameters, createReferralData.R1AddrResp)
-	queryParameters = append(queryParameters, createReferralData.R2PayScale)
-	queryParameters = append(queryParameters, createReferralData.R2ReferralCredit)
-	queryParameters = append(queryParameters, createReferralData.R2ReferralCreditPerc)
-	queryParameters = append(queryParameters, createReferralData.R2AddrResp)
-	queryParameters = append(queryParameters, createReferralData.StartDate)
-	queryParameters = append(queryParameters, createReferralData.EndDate)
+	queryParameters = append(queryParameters, createReferralData.Date)
 
 	// Call the database function
 	result, err = db.CallDBFunction(db.OweHubDbIndex, db.CreateReferralDataFunction, queryParameters)
