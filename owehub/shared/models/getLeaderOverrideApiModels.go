de0bf78490b8f80f6818e3f319cf6ef2cc44e750
Shakezidin <sinuzidin@gmail.com>
2024-07-15 17:14:02 +0530

    leader override start date and enddare issue solved

--- owehub/shared/models/getLeaderOverrideApiModels.go
+++ owehub/shared/models/getLeaderOverrideApiModels.go
@@ -16,7 +16,7 @@ type GetLeaderOverride struct {
 	Qual       string  `json:"qual"`
 	SalesQ     float64 `json:"sales_q"`
 	TeamKwQ    float64 `json:"team_kw_q"`
-	PayRate    string  `json:"pay_rate"`
+	PayRate    float64 `json:"pay_rate"`
 	StartDate  string  `json:"start_date"`
 	EndDate    string  `json:"end_date"`
 }
