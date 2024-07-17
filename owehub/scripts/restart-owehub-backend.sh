91f16fa1032211ea33a721ce64291f9da7171d29
Shushank Sharma <shushank22@gmail.com>
2024-07-17 15:44:14 +0000

    Update build script

--- owehub/scripts/restart-owehub-backend.sh
+++ owehub/scripts/restart-owehub-backend.sh
@@ -11,6 +11,10 @@ rm -rf /home/log/owe/owehub-*
 ./buildApp_Calc.sh make_all
 ./buildApp_Calc.sh make_docker  
 
+./buildApp_Utils.sh make_clean
+./buildApp_Utils.sh make_all
+./buildApp_Utils.sh make_docker  
+
 docker images
 
 ./start-OweApp-Backend-svc.sh
