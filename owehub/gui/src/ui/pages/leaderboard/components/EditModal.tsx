46b23baf53144f1807f03d29f85893be25f55b13
Raman <rajudeveloperw892@gmail.com>
2024-07-19 21:29:29 +0530

    changes

--- owehub/gui/src/ui/pages/leaderboard/components/EditModal.tsx
+++ owehub/gui/src/ui/pages/leaderboard/components/EditModal.tsx
@@ -2,6 +2,9 @@ import './Modal.css';
 import { ICONS } from '../../../icons/Icons';
 import { GoUpload } from 'react-icons/go';
 import { useRef, useState } from 'react';
+import axios from 'axios';
+import { toast } from 'react-toastify';
+ // Adjust the path accordingly
 
 interface EditModalProps {
   onClose: () => void;
@@ -9,7 +12,6 @@ interface EditModalProps {
 
 const EditModal = ({ onClose }: EditModalProps) => {
   const fileInputRef = useRef<HTMLInputElement | null>(null);
-
   const [uploadedImage, setUploadedImage] = useState<{
     src: string;
     file: File;
@@ -24,6 +26,44 @@ const EditModal = ({ onClose }: EditModalProps) => {
       if (typeof reader.result === 'string')
         setUploadedImage({ src: reader.result, file });
     };
+  };
+
+  const uploadImage = async (imageSrc: string): Promise<string> => {
+    if (!imageSrc) throw new Error('No image source provided');
+  
+    try {
+      const blob = await fetch(imageSrc).then((r) => r.blob());
+      const formData = new FormData();
+      formData.append('file', blob);
+      formData.append('upload_preset', 'xdfcmcf4');
+      formData.append('cloud_name', 'duscqq0ii');
+  
+      const response = await axios.post(
+        `https://api.cloudinary.com/v1_1/duscqq0ii/image/upload`,
+        formData
+      );
+      const imageUrl = response.data.secure_url;
+  
+      toast.success('Logo uploaded successfully:', imageUrl);
+      return imageUrl;
+    } catch (error) {
+      console.error('Error uploading image to Cloudinary:', error);
+      throw error;
+    }
+  };
+  
+ 
+
+  const handleUpdate = async () => {
+    if (!uploadedImage?.src) return;
+    try {
+      const imageUrl = await uploadImage(uploadedImage.src);
+      console.log('Uploaded image URL:', imageUrl);
+      // Handle the URL (e.g., save it to your backend or update the state)
+      onClose();
+    } catch (error) {
+      console.error('Error uploading image:', error);
+    }
   };
 
   return (
@@ -60,7 +100,9 @@ const EditModal = ({ onClose }: EditModalProps) => {
           <button className="cancel-button" onClick={onClose}>
             Cancel
           </button>
-          <button className="update-button">Update</button>
+          <button className="update-button" onClick={handleUpdate}>
+            Update
+          </button>
         </div>
       </div>
     </div>
