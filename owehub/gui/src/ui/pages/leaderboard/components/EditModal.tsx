23214531654874c891fd727e2ccba215c5b6166c
shivang991 <32.shivangdubey@gmail.com>
2024-07-19 11:54:53 +0530

    feat: add dark background and upload button in edit leaderboard page

--- owehub/gui/src/ui/pages/leaderboard/components/EditModal.tsx
+++ owehub/gui/src/ui/pages/leaderboard/components/EditModal.tsx
@@ -1,28 +1,66 @@
 import './Modal.css';
 import { ICONS } from '../../../icons/Icons';
 import { GoUpload } from 'react-icons/go';
+import { useRef, useState } from 'react';
 
-//@ts-ignore
-const EditModal = ({onClose}) => {
+interface EditModalProps {
+  onClose: () => void;
+}
+
+const EditModal = ({ onClose }: EditModalProps) => {
+  const fileInputRef = useRef<HTMLInputElement | null>(null);
+
+  const [uploadedImage, setUploadedImage] = useState<{
+    src: string;
+    file: File;
+  } | null>(null);
+
+  const handleFileInputChange = () => {
+    const file = fileInputRef.current?.files?.[0];
+    if (!file) return;
+    const reader = new FileReader();
+    reader.readAsDataURL(file);
+    reader.onload = () => {
+      if (typeof reader.result === 'string')
+        setUploadedImage({ src: reader.result, file });
+    };
+  };
+
   return (
     <div className="edit-modal">
       <div className="leader-modal">
         <h2>Change Picture</h2>
         <div className="modal-center">
-          <object
-            type="image/svg+xml"
-            data={ICONS.BannerLogo}
-            width={150}
-            aria-label="banner-logo"
-          ></object>
-          <div className='upload'>
-            <GoUpload />
+          {uploadedImage ? (
+            <img alt="Preview" src={uploadedImage.src} />
+          ) : (
+            <object
+              type="image/svg+xml"
+              data={ICONS.BannerLogo}
+              width={150}
+              aria-label="banner-logo"
+            ></object>
+          )}
+
+          <input
+            type="file"
+            ref={fileInputRef}
+            accept="image/*"
+            onChange={handleFileInputChange}
+          />
+          <button
+            className="leader-modal-upload"
+            onClick={() => fileInputRef.current?.click()}
+          >
+            <GoUpload size={16} />
             <span>Upload</span>
-          </div>
+          </button>
         </div>
-        <div className='leader-buttons'>
-                <button className='cancel-button' onClick={onClose}>Cancel</button>
-                <button className='update-button'>Update</button>
+        <div className="leader-buttons">
+          <button className="cancel-button" onClick={onClose}>
+            Cancel
+          </button>
+          <button className="update-button">Update</button>
         </div>
       </div>
     </div>
