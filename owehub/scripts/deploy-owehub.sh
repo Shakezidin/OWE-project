11b566d0d9de99c691e038912600c1786a59cb86
Shushank Sharma <shushank22@gmail.com>
2024-07-27 14:37:00 +0000

    Updated deployment script

--- owehub/scripts/deploy-owehub.sh
+++ owehub/scripts/deploy-owehub.sh
@@ -17,23 +17,19 @@ ip_address=$(ip addr show "$interface" | grep -oP '(?<=inet\s)\d+(\.\d+){3}')
 
 # Update IP address in the file (gui/./.env)
 update_ip "$AppRoot/gui/.env" "155.138.163.236" "$ip_address"
-update_ip "$AppRoot/gui/src/config/env_local.json" "155.138.163.236" "$ip_address"
-update_ip "$AppRoot/gui/src/config/env_dev.json" "155.138.163.236" "$ip_address"
 
 # Check if any argument is passed
 if [ $# -eq 0 ]; then
-    echo "No arguments provided. Usage: $0 {deploy_all|gui|backend|database}"
+    echo "No arguments provided. Usage: $0 {all|gui|backend|database}"
     exit 1
 fi
 
 # Check the argument and execute corresponding actions
 case "$1" in
-    deploy_all)
+    all)
         ./restart_ui.sh
-        ./stop-OweApp-Backend-svc.sh
         ./make_postgres_image.sh
         ./restart-owehub-backend.sh
-        ./start-OweApp-Backend-svc.sh
         ;;
     gui)
         ./restart_ui.sh
