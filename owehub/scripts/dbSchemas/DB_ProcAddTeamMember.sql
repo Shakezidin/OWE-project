0d78a93032a8e23ad73bed1fda68b952cc2c60dd
root <raedam786@gmail.com>
2024-07-31 14:22:06 +0000

    updated team member

--- owehub/scripts/dbSchemas/DB_ProcAddTeamMember.sql
+++ owehub/scripts/dbSchemas/DB_ProcAddTeamMember.sql
@@ -1,4 +1,5 @@
-CREATE OR REPLACE FUNCTION update_team_members(
+-- Create or replace the stored procedure to add members and managers to an existing team
+CREATE OR REPLACE FUNCTION add_team_members(
     p_team_id INT,
     p_sale_rep_codes TEXT[],
     p_manager_codes TEXT[]
@@ -11,32 +12,35 @@ DECLARE
     sale_rep_code TEXT;
     manager_code TEXT;
 BEGIN
-    -- Update sale representatives
     FOREACH sale_rep_code IN ARRAY p_sale_rep_codes
-    LOOP
-        -- Fetch user_id based on user_code
-        SELECT user_id INTO sale_rep_id FROM user_details WHERE user_code = sale_rep_code;
+        LOOP
+            SELECT user_id INTO sale_rep_id FROM user_details WHERE user_code = sale_rep_code;
+            INSERT INTO team_members (
+                team_id,
+                user_id,
+                role_in_team
+            )
+            VALUES (
+                p_team_id,
+                sale_rep_id,
+                'member'
+            );
+        END LOOP;
 
-        -- Update team member role
-        UPDATE team_members
-        SET role_in_team = 'member'
-        WHERE team_id = p_team_id
-          AND user_id = sale_rep_id;
-    END LOOP;
-
-    -- Update managers
-    FOREACH manager_code IN ARRAY p_manager_codes
-    LOOP
-        -- Fetch user_id based on user_code
-        SELECT user_id INTO manager_id FROM user_details WHERE user_code = manager_code;
-
-        -- Update team member role
-        UPDATE team_members
-        SET role_in_team = 'manager'
-        WHERE team_id = p_team_id
-          AND user_id = manager_id;
-    END LOOP;
-
+      FOREACH manager_code IN ARRAY p_manager_codes
+        LOOP
+            SELECT user_id INTO manager_id FROM user_details WHERE user_code = manager_code;
+            INSERT INTO team_members (
+                team_id,
+                user_id,
+                role_in_team
+            )
+            VALUES (
+                p_team_id,
+                manager_id,
+                'manager'
+            );
+        END LOOP;
     RETURN 1; -- Indicate success
 END;
 $$ LANGUAGE plpgsql;
