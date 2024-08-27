7d83ac1dcedcd6f3339f312c81ca75751617c447
Shakezidin <sinuzidin@gmail.com>
2024-08-27 17:57:10 +0530

    download csv is added in leaderboard

--- owehub/shared/models/LeaderBoardModels.go
+++ owehub/shared/models/LeaderBoardModels.go
@@ -32,9 +32,10 @@ type GetLeaderBoard struct {
 }
 
 type GetLeaderBoardList struct {
-	LeaderBoardList []GetLeaderBoard `json:"ap_ded_list"`
-	TotalSale       float64          `json:"total_sale"`
-	TotalNtp        float64          `json:"total_ntp"`
-	TotalInstall    float64          `json:"total_install"`
-	TotalCancel     float64          `json:"total_cancel"`
+	LeaderBoardList []GetLeaderBoard         `json:"ap_ded_list"`
+	TotalSale       float64                  `json:"total_sale"`
+	TotalNtp        float64                  `json:"total_ntp"`
+	TotalInstall    float64                  `json:"total_install"`
+	TotalCancel     float64                  `json:"total_cancel"`
+	Data            []map[string]interface{} `json:"data"`
 }
