137580534f94455738a5f98732ce0c2de7cd55ef
Vance-Developer <vance.developer@gmail.com>
2024-08-29 20:01:02 +0530

    performance calendar

--- owehub/gui/src/ui/Calendar/CalendarSidebar.tsx
+++ owehub/gui/src/ui/Calendar/CalendarSidebar.tsx
@@ -18,6 +18,21 @@ const CalendarSidebar: React.FC<CalendarSidebarProps> = ({ onClose, selectedDate
 
     const formattedDate = formatDate(selectedDate);
 
+    const calendarData = [
+        { id: "OUR8636", name: "David Miller", status: "scheduled", title: "Survey", category: "ID", color: "#57B3F1" },
+        { id: "OUR8346", name: "John Robert", status: "completed", title: "Electric", category: "ID", color: "#8E81E0" },
+        { id: "OUR8634", name: "Bratt Mark", status: "scheduled", title: "Install PV", category: "Info", color: "#C470C7" },
+        { id: "OUR6454", name: "David Miller", status: "scheduled", title: "Survey", category: "ID", color: "#57B3F1" },
+        { id: "OUR4453", name: "John Robert", status: "completed", title: "Electric", category: "ID", color: "#8E81E0" },
+        { id: "OUR7453", name: "Bratt Mark", status: "scheduled", title: "Install PV", category: "Info", color: "#C470C7" },
+        { id: "OUR7454", name: "David Miller", status: "scheduled", title: "Survey", category: "ID", color: "#57B3F1" },
+        { id: "OUR2342", name: "John Robert", status: "completed", title: "Electric", category: "ID", color: "#8E81E0" },
+        { id: "OUR5432", name: "Bratt Mark", status: "scheduled", title: "Install PV", category: "Info", color: "#C470C7" },
+        { id: "OUR6343", name: "David Miller", status: "scheduled", title: "Survey", category: "ID", color: "#57B3F1" },
+        { id: "OUR7464", name: "John Robert", status: "completed", title: "Electric", category: "ID", color: "#8E81E0" },
+        { id: "OUR3453", name: "Bratt Mark", status: "scheduled", title: "Install PV", category: "Info", color: "#C470C7" },
+    ]
+
     return (
         <div className="sidebar-wrapper">
             <div className="user-sidebar scrollbar">
@@ -25,7 +40,20 @@ const CalendarSidebar: React.FC<CalendarSidebarProps> = ({ onClose, selectedDate
                     <div className="close-icon" onClick={onClose}><FaArrowRight /></div>
                     <p>{formattedDate}</p>
                 </div>
-               
+                <div className="main-section">
+                    <div className='sidebar-cards'>
+                        {calendarData.map((data) => (
+                            <div className="card">
+                                <p className='card-title' style={{color: data.color}}><span style={{background: data.color}}></span>{data.title} date</p>
+                                <p className='card-id'>Project {data.category}</p>
+                                <p className='our-id'>{data.id}</p>
+                                <p className='card-name'>{data.name}</p>
+                                <button className='card-status' style={{backgroundColor: data.status === "completed" ? "#63ACA3" : "#3C7AF1"}}>{data.status}</button>
+                            </div>
+                        ))}
+                    </div>
+                </div>
+
             </div>
         </div>
     );
