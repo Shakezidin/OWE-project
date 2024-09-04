bdc95a81dc7a77d8f9d33ae89690e9a101ad784a
Shakezidin <sinuzidin@gmail.com>
2024-09-04 17:21:45 +0530

    leader board name updated

--- owehub/owehub-main/services/apiGetLeaderBoardData.go
+++ owehub/owehub-main/services/apiGetLeaderBoardData.go
@@ -161,7 +161,7 @@ func HandleGetLeaderBoardRequest(resp http.ResponseWriter, req *http.Request) {
 		return
 	}
 
-	if dataReq.GroupBy == "dealer" && dataReq.Role != string(types.GroupAdmin) {
+	if dataReq.GroupBy == "dealer" && dataReq.Role != string(types.RoleAdmin) {
 		// Step 1: Extract unique dealer names and prepare query placeholders
 		dealerNames := make([]string, 0, len(data))
 		for _, item := range data {
