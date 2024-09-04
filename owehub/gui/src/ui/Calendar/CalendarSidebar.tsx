fb934ee79293ac257a4b158b890b3a2471f07e5b
Vance-Developer <vance.developer@gmail.com>
2024-09-04 18:04:59 +0530

    calendar changes

--- owehub/gui/src/ui/Calendar/CalendarSidebar.tsx
+++ owehub/gui/src/ui/Calendar/CalendarSidebar.tsx
@@ -19,18 +19,18 @@ const CalendarSidebar: React.FC<CalendarSidebarProps> = ({ onClose, selectedDate
     const formattedDate = formatDate(selectedDate);
 
     const calendarData = [
-        { id: "OUR8636", name: "David Miller", status: "scheduled", title: "Survey", category: "ID", color: "#57B3F1" },
-        { id: "OUR8346", name: "John Robert", status: "completed", title: "Electric", category: "ID", color: "#8E81E0" },
-        { id: "OUR8634", name: "Bratt Mark", status: "scheduled", title: "Install PV", category: "Info", color: "#C470C7" },
-        { id: "OUR6454", name: "David Miller", status: "scheduled", title: "Survey", category: "ID", color: "#57B3F1" },
-        { id: "OUR4453", name: "John Robert", status: "completed", title: "Electric", category: "ID", color: "#8E81E0" },
-        { id: "OUR7453", name: "Bratt Mark", status: "scheduled", title: "Install PV", category: "Info", color: "#C470C7" },
-        { id: "OUR7454", name: "David Miller", status: "scheduled", title: "Survey", category: "ID", color: "#57B3F1" },
-        { id: "OUR2342", name: "John Robert", status: "completed", title: "Electric", category: "ID", color: "#8E81E0" },
-        { id: "OUR5432", name: "Bratt Mark", status: "scheduled", title: "Install PV", category: "Info", color: "#C470C7" },
-        { id: "OUR6343", name: "David Miller", status: "scheduled", title: "Survey", category: "ID", color: "#57B3F1" },
-        { id: "OUR7464", name: "John Robert", status: "completed", title: "Electric", category: "ID", color: "#8E81E0" },
-        { id: "OUR3453", name: "Bratt Mark", status: "scheduled", title: "Install PV", category: "Info", color: "#C470C7" },
+        { id: "OUR8636", name: "David Miller", status: "scheduled", title: "Survey", color: "#57B3F1" },
+        { id: "OUR8346", name: "John Robert", status: "completed", title: "Electric", color: "#8E81E0" },
+        { id: "OUR8634", name: "Bratt Mark", status: "scheduled", title: "Install PV", color: "#C470C7" },
+        { id: "OUR6454", name: "David Miller", status: "scheduled", title: "Survey", color: "#57B3F1" },
+        { id: "OUR4453", name: "John Robert", status: "completed", title: "Electric", color: "#8E81E0" },
+        { id: "OUR7453", name: "Bratt Mark", status: "scheduled", title: "Install PV", color: "#C470C7" },
+        { id: "OUR7454", name: "David Miller", status: "scheduled", title: "Survey", color: "#57B3F1" },
+        { id: "OUR2342", name: "John Robert", status: "completed", title: "Electric", color: "#8E81E0" },
+        { id: "OUR5432", name: "Bratt Mark", status: "scheduled", title: "Install PV", color: "#C470C7" },
+        { id: "OUR6343", name: "David Miller", status: "scheduled", title: "Survey", color: "#57B3F1" },
+        { id: "OUR7464", name: "John Robert", status: "completed", title: "Electric", color: "#8E81E0" },
+        { id: "OUR3453", name: "Bratt Mark", status: "scheduled", title: "Install PV", color: "#C470C7" },
     ]
 
     return (
@@ -44,11 +44,13 @@ const CalendarSidebar: React.FC<CalendarSidebarProps> = ({ onClose, selectedDate
                     <div className='sidebar-cards'>
                         {calendarData.map((data) => (
                             <div className="card">
-                                <p className='card-title' style={{color: data.color}}><span style={{background: data.color}}></span>{data.title} Date</p>
-                                <p className='card-id'>Project {data.category}</p>
-                                <p className='our-id'>{data.id}</p>
-                                <p className='card-name'>{data.name}</p>
-                                <button className='card-status' style={{backgroundColor: data.status === "completed" ? "#63ACA3" : "#3C7AF1"}}>{data.status}</button>
+                                <p className='card-title' style={{ color: data.color }}><span style={{ background: data.color }}></span>{data.title} Date</p>
+                                <div className='flex items-center justify-between' style={{width: "100%"}}>
+                                    <p className='card-name'>{data.name}</p>
+                                    <p className='our-id'>{data.id}</p>
+                                </div>
+                                <a href='#' className='card-address'>102, Malua street, Arizona, 10345</a>
+                                <button className='card-status' style={{ backgroundColor: data.status === "completed" ? "#63ACA3" : "#3C7AF1" }}>{data.status}</button>
                             </div>
                         ))}
                     </div>
