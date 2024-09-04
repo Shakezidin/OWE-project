ccbbb64e5b8d9b4e6bc3745bda3ba0c2b4207d8f
Shakezidin <sinuzidin@gmail.com>
2024-09-04 16:42:55 +0530

    selection specific is done

--- owehub/shared/models/getPendingQueueApiModels.go
+++ owehub/shared/models/getPendingQueueApiModels.go
@@ -7,14 +7,14 @@
 package models
 
 type PendingQueueReq struct {
-	PageNumber   int `json:"page_number"`
-	PageSize     int `json:"page_size"`
-	Email        string
-	UniqueIds    []string `json:"unique_ids"`
-	DealerName   interface{}
-	IntervalDays string
-	StartDate    string `json:"start_date"`
-	EndDate      string `json:"end_date"`
+	PageNumber           int `json:"page_number"`
+	PageSize             int `json:"page_size"`
+	Email                string
+	UniqueIds            []string `json:"unique_ids"`
+	DealerName           interface{}
+	SelectedPendingStage string `json:"selected_pending_stage"`
+	StartDate            string `json:"start_date"`
+	EndDate              string `json:"end_date"`
 }
 
 type GetPendingQueue struct {
