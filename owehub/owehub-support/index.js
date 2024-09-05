2f32d50f2469aaeb586bf19849b7521acb814bcf
Mohd Saquib <mohdsaquib@Mohds-MacBook-Air.local>
2024-09-06 00:06:12 +0530

    owehub-support code

---
+++ owehub/owehub-support/index.js
@@ -0,0 +1,201 @@
+const express = require("express");
+const cors = require("cors");
+
+const { WebClient } = require("@slack/web-api");
+const { App } = require("@slack/bolt");
+const http = require("http");
+const { Server } = require("socket.io");
+
+const createUniqueChannelName = require("./src/utils/createUniqueChannelName");
+const getSupportChannel = require("./src/utils/getSupportChannel");
+
+const { startChatSchema } = require("./src/validations");
+const config = require("./src/config");
+const schedule = require("node-schedule");
+const ms = require("ms");
+
+const app = express();
+const port = process.env.PORT;
+
+app.use(cors({ origin: "*" }));
+// Create an HTTP server for socket.io
+const server = http.createServer(app);
+const io = new Server(server, {
+  cors: { origin: "*" },
+});
+
+const web = new WebClient(config.botToken);
+
+// Database
+const channels = {};
+const userSockets = new Map();
+const jobs = {};
+
+const slackApp = new App({
+  appToken: config.slackAppToken,
+  socketMode: true,
+  token: config.botToken,
+});
+
+slackApp.event("member_joined_channel", async ({ event }) => {
+  console.log("User joined channel:", event);
+});
+
+slackApp.event("channel_joined", async ({ event }) => {
+  console.log("Channel joined:", event);
+});
+
+slackApp.event("channel_deleted", async ({ event }) => {
+  console.log("Channel deleted:", event);
+  if (event.type === "channel_deleted") {
+    delete channels[event.channel];
+    if (!userSockets.get(event.channel)) {
+      console.log("Channel does not exists");
+      return;
+    }
+    io.to(userSockets.get(event.channel)).emit("success", {
+      event_name: "channel_deleted",
+    });
+  }
+});
+
+slackApp.message(async ({ message }) => {
+  console.log(message);
+  if (message.subtype === undefined) {
+    console.log("Message received:", message);
+    const job = jobs[message.channel];
+    if (job) {
+      job.cancel();
+      console.log(jobs);
+      delete jobs[message.channel];
+      console.log(jobs);
+    }
+
+    if (!userSockets.get(message.channel)) {
+      console.log("Channel does not exists");
+      return;
+    }
+    io.to(userSockets.get(message.channel)).emit("success", {
+      event_name: "new_message",
+      message: message.text,
+    });
+  }
+});
+
+slackApp.start();
+
+io.on("connection", (socket) => {
+  console.log("Connection", socket.id);
+  // Handle incoming chat messages from the client
+  socket.on("start-chat", async (data) => {
+    try {
+      console.log("start-chat", data);
+      const { name, email, message, issueType } = data;
+
+      startChatSchema.validateSync(data);
+
+      const channelName = createUniqueChannelName(name, email);
+
+      console.log(channelName);
+
+      // Create a new Slack channel
+      if (!channels[channelName]) {
+        // get info
+        const result = await web.conversations.list();
+
+        // Filter channels by name
+        const channel = result.channels.find((c) => c.name === channelName);
+
+        console.log("Channel List not found", channel);
+        if (channel) {
+          channels[channelName] = channel;
+        } else {
+          const result = await web.conversations.create({
+            name: channelName,
+            is_private: false,
+          });
+
+          channels[channelName] = result.channel;
+        }
+      }
+      console.log(channels);
+      const channelId = channels[channelName].id;
+
+      // Send a message to the created Slack channel
+      await web.chat.postMessage({
+        channel: channelId,
+        text: message,
+      });
+
+      userSockets.set(channelId, socket.id);
+
+      console.log("channelId", channelId);
+
+      // Notify a designated Slack channel about the new message
+      await web.chat.postMessage({
+        channel: getSupportChannel(issueType),
+        text: `A new message has arrived! Join to reply: <#${channelId}|${channelName}>`,
+      });
+
+      const runAt = new Date(Date.now() + ms(config.noReplyRespondTime)); // 2 minutes in the future
+
+      // Schedule the job
+      const job = schedule.scheduleJob(runAt, () => {
+        // Send a message to the created Slack channel
+        socket.emit("success", {
+          event_name: "new_message",
+          message:
+            "Currently, no one availble to respond, we will connect you soon! Thank you for your patience",
+        });
+      });
+
+      jobs[channelId] = job;
+
+      // Emit the message to all connected socket.io clients
+      socket.emit("success", {
+        event_name: "start-chat",
+        message: "Chat Started Successfully",
+        data: {
+          channelName,
+        },
+      });
+    } catch (error) {
+      socket.emit("error", { message: error.message });
+    }
+  });
+
+  // Handle incoming general messages from the client
+  socket.on("send-message", async (data) => {
+    const { message, channelName } = data;
+
+    try {
+      const channelId = channels[channelName]?.id;
+
+      if (!channelId) {
+        throw new Error("No Chanel Found");
+      }
+
+      // Send a message to the created Slack channel
+      await web.chat.postMessage({
+        channel: channelId,
+        text: message,
+      });
+
+      socket.emit("success", {
+        event_name: "send-message",
+        message: "Message Sent Successfully",
+      });
+    } catch (error) {
+      socket.emit("error", { message: error.message });
+    }
+  });
+
+  socket.on("disconnect", () => {
+    console.log("user disconnected");
+  });
+});
+
+// Start the server with socket.io
+server.listen(port, () => {
+  console.log(`Server running at http://localhost:${port}`);
+});
---
+++ owehub/owehub-support/src/validations/index.js
@@ -0,0 +1,21 @@
+const { object, string, number, date, InferType } = require("yup");
+const { issueTypeEnum } = require("./enums");
+
+const configSchema = object({
+  oweDealerSupportChannelId: string().required(),
+  oweSalesSupportChannelId: string().required(),
+  slackAppToken: string().required(),
+  botToken: string().required(),
+});
+
+const startChatSchema = object({
+  issueType: string().oneOf(issueTypeEnum).required(),
+  message: string().required(),
+  email: string().email(),
+  name: string().required(),
+});
+
+module.exports = {
+  startChatSchema,
+  configSchema,
+};
