7e86f57f28899a60ff1e5ba00952c3f03ae15fa9
Raman <rajudeveloperw892@gmail.com>
2024-09-18 20:47:53 +0530

    changes

--- owehub/gui/src/ui/oweHub/projectTracker/ProjectPerformence.tsx
+++ owehub/gui/src/ui/oweHub/projectTracker/ProjectPerformence.tsx
@@ -172,6 +172,32 @@ const ProjectPerformence = () => {
       'Contract $',
       'Sys Size',
       'Sale Date',
+      'Site Survey Schedule Date',
+      'Site Survey Completed Date',
+      'Cad Ready Date',
+      'Cad Completed Date',
+      'Permit Submitted Date',
+      'Ic Submitted Date',
+      'Permit Approved Date',
+      'Ic Approved Date',
+      'Roofing Created Date',
+      'Roofing Complete Date',
+      'Pv Install Created Date',
+      'Battery Scheduled Date',
+      'Battery Completed Date',
+      'Pv Install Completed Date',
+      'Mpu Created Date',
+      'Mpu complete Date',
+      'Derate Create Date',
+      'Derate Complete Date',
+      'Trenching WSOpen Date',
+      'Trenching Complete Date',
+      'FinCreate Date',
+      'FinPass Date',
+      'Pto Submitted Date',
+      'Pto Date'
+
+
     ];
 
     const getAllData = await postCaller('get_peroformancecsvdownload', {
@@ -194,6 +220,35 @@ const ProjectPerformence = () => {
       item.ContractAmount,
       item.SystemSize,
       item.ContractDate,
+      item.SiteSurevyScheduleDate,
+      item.SiteSurveyCompletedDate,
+      item.CadReadyDate,
+      item.CadCompletedDate,
+      item.PermitSubmittedDate,
+      item.IcSubmittedDate,
+      item.PermitApprovedDate,
+      item.IcApprovedDate,
+      item.RoofingCreatedDate,
+      item.RoofingCompleteDate,
+      item.PvInstallCreatedDate,
+      item.MpuCreatedDate,
+      item.MpucompleteDate,
+      item.DerateCreateDate,
+      item.DerateCompleteDate,
+      item.TrenchingWSOpenDate,
+      item.TrenchingCompleteDate,
+      item.FinCreateDate,
+      item.FinPassDate,
+      item.PtoSubmittedDate,
+      item.PtoDate
+      
+      
+
+
+
+      
+      
+      
     ]);
 
     const csvRows = [headers, ...csvData];
