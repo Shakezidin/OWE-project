eaabc70a91ba46b683c1b557f442b88c16c189ce
shivang991 <32.shivangdubey@gmail.com>
2024-09-28 19:24:31 +0530

    feat: add action needed special case in count by status api

--- owehub/owehub-leads/services/apiGetLeadsCountByStatus.go
+++ owehub/owehub-leads/services/apiGetLeadsCountByStatus.go
@@ -81,7 +81,7 @@ func HandleGetLeadsCountByStatusRequest(resp http.ResponseWriter, req *http.Requ
 				ON leads_info.status_id = leads_status.status_id 
 				AND leads_info.updated_at BETWEEN $2 AND $3
 			GROUP BY leads_status.status_id
-			HAVING leads_status.status_id NOT IN (5, 6)
+			HAVING leads_status.status_id NOT IN (5, 6) -- Excluding WON & LOST
 		) grp INNER JOIN leads_status s ON s.status_id = grp.status_id
 	`
 	data, err = db.ReteriveFromDB(db.OweHubDbIndex, query, []interface{}{authenticatedEmail, startDate, endDate})
@@ -107,6 +107,26 @@ func HandleGetLeadsCountByStatusRequest(resp http.ResponseWriter, req *http.Requ
 		if !ok {
 			log.FuncErrorTrace(0, "Failed to assert status_id to int64 type Item: %+v", item)
 			continue
+		}
+
+		// Special case for status_id = 4 (Action Needed)
+		if statusId == 4 {
+			query = `
+				SELECT count(li.*) FROM get_leads_info_hierarchy($1) li
+				WHERE (li.status_id = 5 AND li.proposal_created_date IS NULL)
+				OR (li.status_id NOT IN (5, 6) AND li.appointment_date < CURRENT_TIMESTAMP);
+			`
+
+			data, err = db.ReteriveFromDB(db.OweHubDbIndex, query, []interface{}{authenticatedEmail})
+			if err != nil || len(data) <= 0 {
+				log.FuncErrorTrace(0, "Failed to get action neeeded leads count with err: %v", err)
+			} else {
+				actionNeededCount, ok := data[0]["count"].(int64)
+				if ok {
+					count += actionNeededCount
+				}
+			}
+
 		}
 		apiResponse.Leads = append(apiResponse.Leads, models.GetLeadsCountByStatus{
 			Count:      count,
