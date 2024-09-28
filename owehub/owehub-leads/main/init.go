9745ee6b9e1183b30fa827ed8dd892fc57cec071
root <raedam786@gmail.com>
2024-09-28 06:22:48 +0000

    created lead status win api

--- owehub/owehub-leads/main/init.go
+++ owehub/owehub-leads/main/init.go
@@ -106,6 +106,13 @@ var apiRoutes = ApiRoutes{
 		true,
 		[]types.UserGroup{types.GroupAdminDealer},
 	},
+	{
+		strings.ToUpper("POST"),
+		"/owe-leads-service/v1/status_won",
+		apiHandler.HandleWonRequest,
+		true,
+		[]types.UserGroup{types.GroupAdminDealer},
+	},
 }
 
 /******************************************************************************
