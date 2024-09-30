1ea6fb53e615f14f76c9f37ec561e224ae097a65
rabindr717 <rabindra.kumar@ourworldenergy.com>
2024-09-30 09:34:06 +0530

    Update Font-Size

--- owehub/gui/src/ui/Library/HistoryRedirect.tsx
+++ owehub/gui/src/ui/Library/HistoryRedirect.tsx
@@ -47,6 +47,8 @@ const HistoryRedirect = () => {
       document.removeEventListener('mousedown', handleClickOutside);
     };
   }, []);
+ 
+
 
   useEffect(() => {
     const updateStyles = () => {
