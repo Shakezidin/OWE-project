62320e9d670184dfc93a69dfb87733e01684561f
Ankit Gupta <aryangupta2224@gmail.com>
2024-10-11 12:51:46 +0530

    added type

--- owehub/gui/src/ui/Library/components/FilesTileViewList/FilesTileViewList.tsx
+++ owehub/gui/src/ui/Library/components/FilesTileViewList/FilesTileViewList.tsx
@@ -8,15 +8,16 @@ export interface IFiles {
   webUrl: string;
   size: number;
   "@microsoft.graph.downloadUrl"?: string;
+  mimeType?:string
 
 }
-const FilesTileViewList = ({ files = [],onDelete }: { files?: IFiles[],  onDelete:(id:string)=>void }) => {
+const FilesTileViewList = ({ files = [],onDelete,onFilePreview }: { files?: IFiles[],  onDelete:(id:string)=>void,onFilePreview:(url:string,type:string,name:string)=>void; }) => {
   console.log(files,"filesss")
   return (
     <div className={styles.list_grid}>
       {
         files.map((file) => {
-          return <FileTileView onDelete={(id:string)=>onDelete(id)} file={file} key={file.id} />
+          return <FileTileView onFilePreview={ (url:string,type:string,name:string)=> onFilePreview(url,type,name)} onDelete={(id:string)=>onDelete(id)} file={file} key={file.id} />
         })
       }
 
