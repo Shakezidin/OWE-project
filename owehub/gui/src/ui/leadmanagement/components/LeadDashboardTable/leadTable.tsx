f4f32308303cd6d5b919f971ae2a207507b80578
Vaibhav Chopra <vkcvkc8@gmail.com>
2024-10-19 14:04:37 +0530

    19 OCT View Proposal

--- owehub/gui/src/ui/leadmanagement/components/LeadDashboardTable/leadTable.tsx
+++ owehub/gui/src/ui/leadmanagement/components/LeadDashboardTable/leadTable.tsx
@@ -20,10 +20,11 @@ interface LeadSelectionProps {
   refresh: number;
   setRefresh: (value: number | ((prevValue: number) => number)) => void;
   onCreateProposal: (leadId: number) => void;
-  fetchWebProposal: (leadId: number) => void;
+  retrieveWebProposal: (leadId: number) => void;
+  generateWebProposal: (leadId: number) => void;
 }
 
-const LeadTable = ({ selectedLeads, setSelectedLeads, refresh, setRefresh, onCreateProposal, fetchWebProposal }: LeadSelectionProps) => {
+const LeadTable = ({ selectedLeads, setSelectedLeads, refresh, setRefresh, onCreateProposal, retrieveWebProposal, generateWebProposal }: LeadSelectionProps) => {
 
 
   const [selectedType, setSelectedType] = useState('');
@@ -74,7 +75,15 @@ const LeadTable = ({ selectedLeads, setSelectedLeads, refresh, setRefresh, onCre
     } else if (selectedType === 'new_proposal') {
       onCreateProposal(leadId)
       setSelectedType('');
-    } else if (selectedType === 'Appointment Not Required') {
+    } else if (selectedType === 'viewProposal') {
+      retrieveWebProposal(leadId)
+      setSelectedType('');
+    } else if (selectedType === 'renew_proposal') {
+      generateWebProposal(leadId)
+      setSelectedType('');
+    }
+    
+    else if (selectedType === 'Appointment Not Required') {
       handleAppNotReq();
       setSelectedType('');
     }
@@ -362,11 +371,21 @@ const LeadTable = ({ selectedLeads, setSelectedLeads, refresh, setRefresh, onCre
                             <button className={styles.create_proposal} onClick={handleReschedule}>Reschedule</button>
                           ) : lead?.proposal_id ? (
                             <div className={styles.progress_status}>
-                              <span>Last Updated 2 Days Ago</span>
-                              <p className={styles.prop_send} onClick={() => fetchWebProposal(lead.leads_id)}>
-                              View Proposal <IoChevronForward />
-                                <IoChevronForward style={{marginLeft: "-8px"}}/>
-                              </p>
+                              <span style={{fontSize:11}}>Last Updated: 
+                              <br/>
+                                {new Date(lead.proposal_updated_at).toLocaleString('en-GB', {
+                                  year: 'numeric', month: 'long', day: 'numeric'
+                              })}</span>
+                              <br/>
+                              <a 
+                                  href={lead.proposal_link} 
+                                  target="_blank" 
+                                  rel="noopener noreferrer" 
+                                  className="view-proposal-button"
+                                  style={{color:"#377CF6",fontWeight:550,alignItems: "center", display:"flex"}}
+                              >
+                                  View Proposal <IoChevronForward /><IoChevronForward style={{marginLeft: "-8px"}}/>
+                              </a>
                             </div>
                           ) :
                             (lead.appointment_status_label === "Not Required" || (lead.proposal_id === "" && lead.appointment_status_label !== "")) ? (
@@ -390,7 +409,7 @@ const LeadTable = ({ selectedLeads, setSelectedLeads, refresh, setRefresh, onCre
                                     : lead && lead.proposal_status && lead.proposal_status.toLowerCase() === 'completed' && lead.proposal_id !== ''
                                       ? [
                                           { label: 'View Proposal', value: 'viewProposal' },
-                                          { label: 'Recreate Proposal', value: 'new_proposal' },
+                                          { label: 'Recreate Proposal', value: 'renew_proposal' },
                                           { label: 'Download Proposal', value: 'download' },
                                           { label: 'Reschedule Appointment', value: 'app_sched' },
                                         ]
