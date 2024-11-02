1735ca7693e7e1ab1953bf8526607ec6f38d5f3b
rabindr717 <rabindra.kumar@ourworldenergy.com>
2024-11-02 12:02:39 +0530

    New page Added

--- owehub/gui/src/ui/leadmanagement/Modals/SignDocument.tsx
+++ owehub/gui/src/ui/leadmanagement/Modals/SignDocument.tsx
@@ -8,45 +8,25 @@ const SignDocument=()=>{
     <tr>
     <td className={classes.leftAlign}>First Name</td>
     <td
-        style={{
-            whiteSpace: 'pre-wrap',
-            overflowWrap: 'break-word',
-            maxWidth: '200px',
-            lineHeight: "16px"
-        }}
+       
         className={classes.rightAlign}>RABINDRA</td>
 </tr>
 <tr>
     <td className={classes.leftAlign}>Last Name</td>
     <td
-        style={{
-            whiteSpace: 'pre-wrap',
-            overflowWrap: 'break-word',
-            maxWidth: '200px',
-            lineHeight: "16px"
-        }}
+       
         className={classes.rightAlign}>SHARMA</td>
 </tr>
 <tr>
     <td className={classes.leftAlign}>Email Id</td>
     <td
-        style={{
-            whiteSpace: 'pre-wrap',
-            overflowWrap: 'break-word',
-            maxWidth: '200px',
-            lineHeight: "16px"
-        }}
+       
         className={classes.rightAlign}>gmail.com</td>
 </tr>
 <tr>
     <td className={classes.leftAlign}>Phone Number</td>
     <td
-        style={{
-            whiteSpace: 'pre-wrap',
-            overflowWrap: 'break-word',
-            maxWidth: '200px',
-            lineHeight: "16px"
-        }}
+        
         className={classes.rightAlign}>867983274897</td>
 </tr></tbody></table></div></>
 }
