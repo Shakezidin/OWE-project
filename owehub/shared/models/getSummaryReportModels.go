a7422a4b2378a9eb19c7d13110d9ac2896faffa4
root <raedam786@gmail.com>
2024-12-24 16:07:02 +0000

    model cleanup

--- owehub/shared/models/getSummaryReportModels.go
+++ owehub/shared/models/getSummaryReportModels.go
@@ -6,44 +6,12 @@
 
 package models
 
-import "time"
-
 type SummaryReportRequest struct {
 	Year       string   `json:"year"`
 	Week       string   `json:"week"`
 	Day        string   `json:"day"`
 	ReportType string   `json:"report_type"`
 	Office     []string `json:"office"`
-}
-
-type ProductionSummaryReportResponse struct {
-	Home_Owner     string    `json:"home_owner"`
-	Current_Status string    `json:"current_status"`
-	Unique_ID      string    `json:"unique_id"`
-	Dealer_Code    string    `json:"dealer_code"`
-	Sys_Size       float64   `json:"sys_size"`
-	Contract       float64   `json:"contract"`
-	Other_Adders   float64   `json:"other_adders"`
-	Rep1           string    `json:"rep1"`
-	Rep2           string    `json:"rep2"`
-	Setter         string    `json:"setter"`
-	ST             string    `json:"st"`
-	Contract_Date  time.Time `json:"contract_date"`
-	Loan_Fee       float64   `json:"loan_fee"`
-	Net_EPC        float64   `json:"net_epc"`
-	Credit         string    `json:"credit"`
-	Draw_Amt       float64   `json:"draw_amt"`
-	RL             float64   `json:"rl"`
-	Type           string    `json:"finance_type"`
-	Today          time.Time `json:"today"`
-	Amount         float64   `json:"amount"`
-	EPC            float64   `json:"epc"`
-	Amt_Paid       float64   `json:"amt_paid"`
-	Balance        float64   `json:"balance"`
-	Adder          Adder     `json:"adder"`
-	Watt           float64   `json:"watt"`
-	Commissions    float64   `json:"commissions"`
-	Paid           float64   `json:"paid"`
 }
 
 type DataPoint struct {
