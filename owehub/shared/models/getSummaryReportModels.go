a4140509155c8689e795377cd9153a2c7b29f4c5
shivam_petwal <petwalshivam@gmail.com>
2024-12-24 22:13:50 +0530

    dummy data

---
+++ owehub/shared/models/getSummaryReportModels.go
@@ -0,0 +1,29 @@
+/**************************************************************************
+ *	Function	: getDealerPayCommApiModels.go
+ *	DESCRIPTION : Files contains struct for summary report models
+ *	DATE        : 22-Dec-2024
+ **************************************************************************/
+
+ package models
+
+ type SummaryReportRequest struct {
+	 Year       string   `json:"year"`
+	 Week       string   `json:"week"`
+	 ReportType string   `json:"report_type"`
+	 Office     []string `json:"office"`
+ }
+
+ type SaleToInstallSpeedSummaryReportRequest struct {
+	 Year            string   `json:"year"`
+	 Week            string   `json:"week"`
+	 BatteryIncluded string   `json:"batteryincluded"`
+	 Office          []string `json:"office"`
+ }
+
+ type DataPoint struct {
+	 Value map[string]float64 `json:"value"`
+ }
+
+ type OverallSpeedSummaryReportResponse struct {
+	 Data map[string][]DataPoint `json:"data"`
+ }
