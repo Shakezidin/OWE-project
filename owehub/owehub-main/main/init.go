a4fb6b1779b10d35e8d73f56da89e8bddaeaafec
shivang991 <32.shivangdubey@gmail.com>
2024-12-30 15:16:16 +0530

    fix: add relevent mapping sales partner db hub upsertion

--- owehub/owehub-main/main/init.go
+++ owehub/owehub-main/main/init.go
@@ -622,12 +622,11 @@ func init() {
 		log.ConfDebugTrace(0, "Database Configuration fatched Successfully from file.")
 	}
 
-	/* Upsert sales partners from owe db on service start */
-	// err = apiHandler.UpsertSalesPartnersFromOweDb()
-	// if err != nil {
-	// 	log.FuncErrorTrace(0, "Failed to pull sales partners from owe db with error = %v", err)
-	// 	// return
-	// }
+	/* Upsert sales partners from owe db on service start; but don't exit if it fails */
+	salesPartnerErr := apiHandler.UpsertSalesPartnersFromOweDb()
+	if salesPartnerErr != nil {
+		log.FuncErrorTrace(0, "Failed to pull sales partners from owe db with error = %v", salesPartnerErr)
+	}
 
 	//* Read and Initialize Podio configuration from cfg */
 	// err = FetchPodioCfg()
