984b99a9386fe352127fec4e7db5cd2b3b5864fe
root <raedam786@gmail.com>
2025-01-06 10:46:16 +0000

    added new apis [apiEditReport + apiGetReportById ]

--- owehub/owehub-reports/services/apiEditReport.go
+++ owehub/owehub-reports/services/apiEditReport.go
@@ -1,90 +1,112 @@
 /**************************************************************************
- * File       	   : apiEditReport.go
- * DESCRIPTION     : This file contains functions to edit superset report
- * DATE            : 6-Jan-2025
- **************************************************************************/
+* File			: apiEditReport.go
+* DESCRIPTION	: This file contains functions for EditReport handler
+* DATE			: 6-Jan-2025
+**************************************************************************/
 
- package services
+package services
 
- import (
-		 "OWEApp/shared/appserver"
-		 "OWEApp/shared/db"
-		 log "OWEApp/shared/logger"
-		 models "OWEApp/shared/models"
-		 "encoding/json"
-		 "fmt"
-		 "io/ioutil"
-		 "net/http"
-		 //"strings"
- )
+import (
+    "OWEApp/shared/appserver"
+    "OWEApp/shared/db"
+    log "OWEApp/shared/logger"
+    models "OWEApp/shared/models"
+    "encoding/json"
+    "fmt"
+    "io/ioutil"
+    "net/http"
+    "strings"
+)
 
- /******************************************************************************
-	* FUNCTION:				HandleEditReportRequest
-	* DESCRIPTION:     handler for edit report request
-	* INPUT:						resp, req
-	* RETURNS:    			void
-	******************************************************************************/
- func HandleEditReportRequest(resp http.ResponseWriter, req *http.Request) {
-		 var (
-				 err     error
-				 query   string
-				 dataReq models.EditReportReq
-		 )
+/******************************************************************************
+ * FUNCTION:		HandleEditReportRequest
+ * DESCRIPTION:     handler for EditReport request
+ * INPUT:			resp, req
+ * RETURNS:    		void
+ ******************************************************************************/
+func HandleEditReportRequest(resp http.ResponseWriter, req *http.Request) {
+    var (
+        err          error
+        whereEleList []interface{}
+        updateFields []string
+        dataReq      models.EditReportReq
+        query        string
+    )
 
-		 log.EnterFn(0, "HandleEditReportRequest")
-		 defer func() { log.ExitFn(0, "HandleEditReportRequest", err) }()
+    log.EnterFn(0, "HandleEditReportRequest")
+    defer func() { log.ExitFn(0, "HandleEditReportRequest", err) }()
 
-		 if req.Body == nil {
-				 err = fmt.Errorf("HTTP Request body is null")
-				 log.FuncErrorTrace(0, "%v", err)
-				 appserver.FormAndSendHttpResp(resp, "HTTP Request body is null", http.StatusBadRequest, nil)
-				 return
-		 }
+    if req.Body == nil {
+        err = fmt.Errorf("HTTP Request body is null in update request")
+        log.FuncErrorTrace(0, "%v", err)
+        appserver.FormAndSendHttpResp(resp, "HTTP Request body is null", http.StatusBadRequest, nil)
+        return
+    }
 
-		 reqBody, err := ioutil.ReadAll(req.Body)
-		 if err != nil {
-				 log.FuncErrorTrace(0, "Failed to read HTTP Request Body err: %v", err)
-				 appserver.FormAndSendHttpResp(resp, "Failed to read HTTP Request body", http.StatusBadRequest, nil)
-				 return
-		 }
+    reqBody, err := ioutil.ReadAll(req.Body)
+    if err != nil {
+        log.FuncErrorTrace(0, "Failed to read HTTP Request body: %v", err)
+        appserver.FormAndSendHttpResp(resp, "Failed to read HTTP Request body", http.StatusBadRequest, nil)
+        return
+    }
 
-		 err = json.Unmarshal(reqBody, &dataReq)
-		 if err != nil {
-				 log.FuncErrorTrace(0, "Failed to unmarshal HTTP Request Body err: %v", err)
-				 appserver.FormAndSendHttpResp(resp, "Failed to unmarshal HTTP Request Body", http.StatusBadRequest, nil)
-				 return
-		 }
+    err = json.Unmarshal(reqBody, &dataReq)
+    if err != nil {
+        log.FuncErrorTrace(0, "Failed to unmarshal request: %v", err)
+        appserver.FormAndSendHttpResp(resp, "Failed to unmarshal request", http.StatusBadRequest, nil)
+        return
+    }
 
-		 query = fmt.Sprintf(`
-				 UPDATE %s
-				 SET
-						 category = '%s',
-						 title = '%s',
-						 subtitle = '%s',
-						 dashboard_id = '%s'
-				 WHERE
-						 id = %d
-		 `, db.TableName_SupersetReports, dataReq.Category, dataReq.Title, dataReq.Subtitle, dataReq.DashboardId, dataReq.ReportId)
+    // Validate ReportId check
+    if dataReq.ReportId <= 0 {
+        err = fmt.Errorf("invalid report ID %d", dataReq.ReportId)
+        log.FuncErrorTrace(0, "%v", err)
+        appserver.FormAndSendHttpResp(resp, "Invalid report ID, update failed", http.StatusBadRequest, nil)
+        return
+    }
 
+    if len(dataReq.Category) > 0 {
+        whereEleList = append(whereEleList, dataReq.Category)
+        updateFields = append(updateFields, fmt.Sprintf("category = $%d", len(whereEleList)))
+    }
 
+    if len(dataReq.Title) > 0 {
+        whereEleList = append(whereEleList, dataReq.Title)
+        updateFields = append(updateFields, fmt.Sprintf("title = $%d", len(whereEleList)))
+    }
 
-		 err, res := db.UpdateDataInDB(db.OweHubDbIndex, query, nil)
-		 if err != nil {
-				 log.FuncErrorTrace(0, "Failed to update superset report in DB err: %v", err)
-				 appserver.FormAndSendHttpResp(resp, "Failed to update superset report", http.StatusInternalServerError, nil)
-				 return
-		 }
+    if len(dataReq.Subtitle) > 0 {
+        whereEleList = append(whereEleList, dataReq.Subtitle)
+        updateFields = append(updateFields, fmt.Sprintf("subtitle = $%d", len(whereEleList)))
+    }
 
-		 if err != nil {
-			log.FuncErrorTrace(0, "Failed to edit report : %v", err)
-			appserver.FormAndSendHttpResp(resp, "Failed to edit report", http.StatusInternalServerError, nil)
-			return
-		}
+    if len(dataReq.DashboardId) > 0 {
+        whereEleList = append(whereEleList, dataReq.DashboardId)
+        updateFields = append(updateFields, fmt.Sprintf("dashboard_id = $%d", len(whereEleList)))
+    }
 
-		if res == 0 {
-			log.FuncErrorTrace(0, "No rows updated for report id: %v", err)
-			appserver.FormAndSendHttpResp(resp, "No rows were updated", http.StatusInternalServerError, nil)
-			return
-		}
-		appserver.FormAndSendHttpResp(resp, "report updated successfully.", http.StatusOK, nil)
-	}
+    if len(updateFields) == 0 {
+        err = fmt.Errorf("no fields provided to update")
+        log.FuncErrorTrace(0, "%v", err)
+        appserver.FormAndSendHttpResp(resp, "No fields provided to update", http.StatusBadRequest, nil)
+        return
+    }
+
+    whereEleList = append(whereEleList, dataReq.ReportId)
+    query = fmt.Sprintf("UPDATE %s SET %s WHERE id = $%d", db.TableName_SupersetReports, strings.Join(updateFields, ", "), len(whereEleList))
+
+    err, res := db.UpdateDataInDB(db.OweHubDbIndex, query, whereEleList)
+
+    if err != nil {
+        log.FuncErrorTrace(0, "Failed to update report info: %v", err)
+        appserver.FormAndSendHttpResp(resp, "Failed to update report info", http.StatusInternalServerError, nil)
+        return
+    }
+
+    if res == 0 {
+        log.FuncErrorTrace(0, "No rows updated for report details: %v", err)
+        appserver.FormAndSendHttpResp(resp, "No rows were updated", http.StatusInternalServerError, nil)
+        return
+    }
+    appserver.FormAndSendHttpResp(resp, "Report info updated successfully.", http.StatusOK, nil)
+}
