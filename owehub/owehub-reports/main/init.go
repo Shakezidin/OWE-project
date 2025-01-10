15c9249e94cd204fe30a30f0160893c092aec8ed
shivang991 <32.shivangdubey@gmail.com>
2025-01-10 15:22:23 +0530

    feat: create dummy api for get reports

--- owehub/owehub-reports/main/init.go
+++ owehub/owehub-reports/main/init.go
@@ -137,13 +137,13 @@ var apiRoutes = appserver.ApiRoutes{
 		false,
 		[]types.UserGroup{types.GroupAdmin},
 	},
-	// {
-	// 	strings.ToUpper("POST"),
-	// 	"/owe-reports-service/v1/get_reports_achived",
-	// 	apiHandler.HandleReportsAchivedListRequest,
-	// 	false,
-	// 	[]types.UserGroup{types.GroupAdmin},
-	// },
+	{
+		strings.ToUpper("POST"),
+		"/owe-reports-service/v1/get_reports_achived",
+		apiHandler.HandleReportsTargetListRequest,
+		true,
+		[]types.UserGroup{types.GroupAdmin},
+	},
 }
 
 /******************************************************************************
--- owehub/shared/models/getReportsTargetModels.go
+++ owehub/shared/models/getReportsTargetModels.go
@@ -6,10 +6,34 @@
 
 package models
 
-type GetReportsTargetModelsReq struct {
-	TargetType int64  `json:"target_type"`
+type GetReportsTargetReq struct {
+	TargetType string `json:"target_type"`
 	Month      string `json:"month"`
 	Year       string `json:"year"`
+}
+
+type GetReportsTargetRespSummaryItem struct {
+	Target            interface{} `json:"target"`
+	Achieved          interface{} `json:"achieved"`
+	LastMonthAcheived float64     `json:"last_month_acheived"`
+}
+
+type GetReportsTargetRespProgressItem struct {
+	Target             interface{} `json:"target"`
+	Achieved           interface{} `json:"achieved"`
+	PercentageAchieved float64     `json:"percentage_achieved"`
+}
+
+type GetReportsTargetRespMonthlyItem struct {
+	Month    string      `json:"month"`
+	Target   interface{} `json:"target"`
+	Achieved interface{} `json:"achieved,omitempty"`
+}
+
+type GetReportsTargetResp struct {
+	Summary         map[string]GetReportsTargetRespSummaryItem   `json:"summary"`
+	Progress        map[string]GetReportsTargetRespProgressItem  `json:"progress"`
+	MonthlyOverview map[string][]GetReportsTargetRespMonthlyItem `json:"monthly_overview"`
 }
 
 // type MonthlyData struct {
@@ -21,47 +45,47 @@ type GetReportsTargetModelsReq struct {
 // 	Incomplete     *int   `json:"Incomplete,omitempty"`       // Incomplete value (optional)
 // }
 
-type GetReportsTargetModel struct {
-	MileStone             string `json:"mile_stone"`
-	YearlyAchived         int64  `json:"yearly_achived"`
-	YearlyTarget          string `json:"yearly_target"`
-	LastMonthTargetStatus string `json:"last_month_target_status"`
-	MonthlyAchived        string `json:"monthly_achived"`
-	MonthlyTarget         string `json:"monthly_target"`
-	MonthlyTargetPerc     string `json:"monthly_target_perc"`
-	// MonthlyDatas          []MonthlyData `json:"monthly_datas"`
-}
+// type GetReportsTargetModel struct {
+// 	MileStone             string `json:"mile_stone"`
+// 	YearlyAchived         int64  `json:"yearly_achived"`
+// 	YearlyTarget          string `json:"yearly_target"`
+// 	LastMonthTargetStatus string `json:"last_month_target_status"`
+// 	MonthlyAchived        string `json:"monthly_achived"`
+// 	MonthlyTarget         string `json:"monthly_target"`
+// 	MonthlyTargetPerc     string `json:"monthly_target_perc"`
+// 	// MonthlyDatas          []MonthlyData `json:"monthly_datas"`
+// }
 
-type MilestoneTarget struct {
-	Month                string
-	TargetProjectsSold   float64
-	TargetMwSold         float64
-	TargetInstallCount   float64
-	TargetMwInstalled    float64
-	TargetBatteriesCount float64
-}
+// type MilestoneTarget struct {
+// 	Month                string
+// 	TargetProjectsSold   float64
+// 	TargetMwSold         float64
+// 	TargetInstallCount   float64
+// 	TargetMwInstalled    float64
+// 	TargetBatteriesCount float64
+// }
 
-type MonthlySale struct {
-	Month       string
-	SaleCount   float64
-	KwSaleCount float64
-}
+// type MonthlySale struct {
+// 	Month       string
+// 	SaleCount   float64
+// 	KwSaleCount float64
+// }
 
-type MonthlyAchievement struct {
-	Month                  string  `json:"month"`                    // Month (e.g., "Jan", "Feb", "Mar")
-	ActualSaleCount        float64 `json:"actual_sale_count"`        // Actual sale count for the month
-	ActualKwSaleCount      float64 `json:"actual_kw_sale_count"`     // Actual KW sale count for the month
-	TargetProjectsSold     float64 `json:"target_projects_sold"`     // Target projects sold for the month
-	TargetMwSold           float64 `json:"target_mw_sold"`           // Target MW sold for the month
-	PercentageSaleAchieved float64 `json:"percentage_sale_achieved"` // Percentage of sale target achieved for the month
-	PercentageKwAchieved   float64 `json:"percentage_kw_achieved"`   // Percentage of KW target achieved for the month
-}
+// type MonthlyAchievement struct {
+// 	Month                  string  `json:"month"`                    // Month (e.g., "Jan", "Feb", "Mar")
+// 	ActualSaleCount        float64 `json:"actual_sale_count"`        // Actual sale count for the month
+// 	ActualKwSaleCount      float64 `json:"actual_kw_sale_count"`     // Actual KW sale count for the month
+// 	TargetProjectsSold     float64 `json:"target_projects_sold"`     // Target projects sold for the month
+// 	TargetMwSold           float64 `json:"target_mw_sold"`           // Target MW sold for the month
+// 	PercentageSaleAchieved float64 `json:"percentage_sale_achieved"` // Percentage of sale target achieved for the month
+// 	PercentageKwAchieved   float64 `json:"percentage_kw_achieved"`   // Percentage of KW target achieved for the month
+// }
 
-type ReportTargetResponse struct {
-	MonthlySales       []MonthlySale        `json:"monthly_sale"`         // List of monthly sales data
-	TotalSaleCount     float64              `json:"total_sale_count"`     // Total sale count for the year
-	TotalKwSaleCount   float64              `json:"total_kw_sale_count"`  // Total KW sale count for the year
-	TargetSaleCount    float64              `json:"target_sale_count"`    // Total target sale count for the year
-	TargetKwSaleCount  float64              `json:"target_kw_sale_count"` // Total target KW sale count for the year
-	MonthlyAchievement []MonthlyAchievement `json:"monthly_achievement"`  // List of monthly achievement data
-}
+// type ReportTargetResponse struct {
+// 	MonthlySales       []MonthlySale        `json:"monthly_sale"`         // List of monthly sales data
+// 	TotalSaleCount     float64              `json:"total_sale_count"`     // Total sale count for the year
+// 	TotalKwSaleCount   float64              `json:"total_kw_sale_count"`  // Total KW sale count for the year
+// 	TargetSaleCount    float64              `json:"target_sale_count"`    // Total target sale count for the year
+// 	TargetKwSaleCount  float64              `json:"target_kw_sale_count"` // Total target KW sale count for the year
+// 	MonthlyAchievement []MonthlyAchievement `json:"monthly_achievement"`  // List of monthly achievement data
+// }
