c3b1e8ca0a4af549aa8e3270821ad824880ebaec
ShamsTanweer <shamstanweer27@gmail.com>
2025-01-14 16:58:10 +0530

    Updated users tables.

--- owehub/gui/src/ui/oweHub/userManagement/userManagerAllTable/AccountManagerTable.tsx
+++ owehub/gui/src/ui/oweHub/userManagement/userManagerAllTable/AccountManagerTable.tsx
@@ -166,6 +166,8 @@ const AccountManagerTable: React.FC<UserTableProps> = ({
                 )}
                 <td>{el.email_id}</td>
                 <td>{el.mobile_number}</td>
+                <td>{el.reporting_manager ? el.reporting_manager : 'NA'}</td>
+                <td>{el.dealer ? el.dealer : 'NA'}</td>
                 <td style={{ whiteSpace: 'pre-wrap', wordBreak: 'break-word' }}>
                   {el.description ? el.description : 'NA'}
                 </td>
