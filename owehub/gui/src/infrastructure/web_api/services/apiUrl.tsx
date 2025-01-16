803dbd2c5252e08a3f9493c4c51be7a7bec430c2
Raman <rajudeveloperw892@gmail.com>
2025-01-16 19:28:35 +0530

    changes

--- owehub/gui/src/infrastructure/web_api/services/apiUrl.tsx
+++ owehub/gui/src/infrastructure/web_api/services/apiUrl.tsx
@@ -24,13 +24,14 @@ export interface LoginResponse {
   message: string;
 }
 
-// Logout utility function
+ // Logout utility function
 const logoutUser = () => {
- // Clear the token
+  localStorage.removeItem('token'); // Clear the token
   localStorage.removeItem('userName');
-  console.log("check logout")
-   // Redirect to the login page
+  localStorage.removeItem('role');
+  window.location.href = '/login'; // Redirect to the login page
 };
+
 
 export const login = async (
   credentials: Credentials
