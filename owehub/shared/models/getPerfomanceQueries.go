10983088cba5d9095409ecc1bc4b4f07f1b85f20
Shushank Sharma <shushank22@gmail.com>
2025-02-04 21:31:13 +0000

    changed to query

--- owehub/shared/models/getPerfomanceQueries.go
+++ owehub/shared/models/getPerfomanceQueries.go
@@ -1119,7 +1119,7 @@ func PipelineDealerDataQuery(filterUserQuery string) string {
     )
     SELECT
         -- Customer Basic Information
-        -- DISTINCT (cust.unique_id),
+        DISTINCT (cust.unique_id),
         cust.customer_name AS customer_name,
         cust.dealer AS partner_dealer,
         cust.finance_company AS finance_company,
