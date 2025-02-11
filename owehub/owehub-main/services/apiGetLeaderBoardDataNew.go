b402a8b8adbaa26b5335455ad31a65d587e0d96b
Shakezidin <sinuzidin@gmail.com>
2025-02-11 15:24:33 +0530

    leaderboard issue solved

--- owehub/owehub-main/services/apiGetLeaderBoardDataNew.go
+++ owehub/owehub-main/services/apiGetLeaderBoardDataNew.go
@@ -223,6 +223,8 @@ func HandleGetLeaderBoardRequest(resp http.ResponseWriter, req *http.Request) {
 		LeaderBoardList.TopLeaderBoardList = Paginate(filteredResults, 1, 3)
 		LeaderBoardList.LeaderBoardList = Paginate(filteredResults, dataReq.PageNumber, dataReq.PageSize)
 	} else {
+		LeaderBoardList.TopLeaderBoardList = []models.CombinedResult{}
+		LeaderBoardList.LeaderBoardList = []models.CombinedResult{}
 		appserver.FormAndSendHttpResp(resp, "LeaderBoard Data", http.StatusOK, LeaderBoardList, RecordCount)
 		return
 	}
