5ad22ecd790a592122abeaf66ad1ead9858b106b
Raman <rajudeveloperw892@gmail.com>
2025-02-13 11:18:36 +0530

    superset removed

--- owehub/gui/src/ui/reporting/InstallToFIN/index.tsx
+++ owehub/gui/src/ui/reporting/InstallToFIN/index.tsx
@@ -1,9 +1,18 @@
-import SupersetDashboard from '../components/SupersetDashboard';
+// import SupersetDashboard from '../components/SupersetDashboard';
 
-const InstallToFin = () => {
+// const InstallToFin = () => {
+//   return (
+//     <SupersetDashboard dashboardId="a7a96a1e-56d5-45ec-8fff-7bc9ec95f835" />
+//   );
+// };
+
+// export default InstallToFin;
+import React from 'react'
+
+const index = () => {
   return (
-    <SupersetDashboard dashboardId="a7a96a1e-56d5-45ec-8fff-7bc9ec95f835" />
-  );
-};
+ <div></div>
+  )
+}
 
-export default InstallToFin;
+export default index
