68b6067183c5ee4349929368fa8b4439591bf43a
Vance-Developer <vance.developer@gmail.com>
2025-02-27 22:35:59 +0530

    pending actions table changes

---
+++ owehub/gui/src/resources/static_data/PendingActionColumn.ts
@@ -0,0 +1,86 @@
+export default [
+    {
+      name: 'unique_id',
+      displayName: 'Project ID',
+      type: 'string',
+      isCheckbox: false,
+    },
+    {
+      name: 'ntp.production',
+      displayName: 'Production',
+      type: 'string',
+      isCheckbox: false,
+    },
+    {
+      name: 'ntp.finance_NTP',
+      displayName: 'Finance NTP',
+      type: 'string',
+      isCheckbox: false,
+    },
+    {
+      name: 'ntp.utility_bill',
+      displayName: 'Utility Bill',
+      type: 'string',
+      isCheckbox: false,
+    },
+    {
+      name: 'ntp.powerclerk',
+      displayName: 'Power Clerk',
+      type: 'string',
+      isCheckbox: false,
+    },
+    {
+      name: 'contract_date',
+      displayName: 'Sold Date',
+      type: 'date',
+      isCheckbox: false,
+      filter: 'sale_date',
+    },
+    {
+      name: 'app_status',
+      displayName: 'App Status',
+      type: 'string',
+      isCheckbox: false,
+    },
+    {
+      name: 'project_status',
+      displayName: 'Project Status',
+      type: 'string',
+      isCheckbox: false,
+    },
+    {
+      name: 'rep_1',
+      displayName: 'Sales Rep',
+      type: 'string',
+      isCheckbox: false,
+      filter: 'primary_sales_rep',
+    },
+    {
+      name: 'rep_2',
+      displayName: 'Setter',
+      type: 'string',
+      isCheckbox: false,
+      filter: 'secondary_sales_rep',
+    },
+    {
+      name: 'loan_type',
+      displayName: 'Deal Type',
+      type: 'string',
+      isCheckbox: false,
+      filter: 'finance_type',
+    },
+    {
+      name: 'ntp_complete_date',
+      displayName: 'NTP Date',
+      type: 'date',
+      isCheckbox: false,
+    },
+    {
+      name: 'days_project_age',
+      displayName: 'Project Age (days)',
+      type: 'string',
+      isCheckbox: false,
+      isNotFilter: true,
+    },
+  ];
+  
